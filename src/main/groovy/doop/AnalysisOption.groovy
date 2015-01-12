0577735fceb4170305e003bce1f32a322ee615d5
George Kastrinis <george.kast@gmail.com>
2015-01-13 01:20:28 +0200

    Switching to /bin/bash instead of /bin/sh. Ignore the warning message of 3.10

---
+++ src/main/groovy/doop/AnalysisOption.groovy
@@ -0,0 +1,69 @@
+package doop
+/**
+ * @author: Kostas Saidis (saiko@di.uoa.gr)
+ * Date: 19/7/2014
+ *
+ * A DOOP analysis option
+ */
+class AnalysisOption<T>  {
+
+    /**
+     * The id of the option as used internally by the code (e.g. by the preprocessor, the web form, etc)
+     */
+    String id
+
+    /**
+     * The description of the option (for the end-user)
+     */
+    String description
+
+    /**
+     * The value of the option
+     */
+    T value
+
+    /**
+     * Indicates whether the option affects the preprocessor
+     */
+    boolean forPreprocessor = false
+	
+	/**
+	 * The type of the preprocessor flag (ignored when forPreprocessor is false)
+	 */
+	PreprocessorFlag flagType = PreprocessorFlag.NORMAL_FLAG
+
+    /**
+     * Indicates whether the option can be specified by the user in the web UI
+     */
+    boolean webUI = false
+
+    /**
+     * The name of the option (for the end-user)
+     */
+    String name = null
+
+    /**
+     * Indicates whether the option can be specified by the user in the command line interface
+     */
+    boolean cli = false
+
+    /**
+     * The name of the option's arg value. If null, the option does not take arguments (it is a flag/boolean option).
+     */
+    String argName = null
+	
+	/**
+	 * Indicates whether the option is "advanced". Advanced options are treated differently by the UIs.
+	 */
+	boolean isAdvanced = false
+
+    /**
+     * Indicates whether the options is a file.
+     */
+    boolean isFile = false
+
+    @Override
+    String toString() {
+        return getId() + "=" + getValue()
+    }
+}
---
+++ src/main/java/doop/soot/CSVDatabase.java
@@ -0,0 +1,142 @@
+package doop.soot;
+
+import doop.PredicateFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.Map;
+
+import static doop.PredicateFile.ImportScheme.BY_FILE_PREDICATE;
+
+public class CSVDatabase implements Database
+{
+    /** The maximum number of characters per column */
+    private static final int MAX_COLUMN_LENGTH = 256;
+
+    private static final char SEP = '\t';
+    private static final char EOL = '\n';
+
+    private File directory;
+    private Map<PredicateFile, Writer> writers;
+
+    public CSVDatabase(File directory) throws IOException
+    {
+        this.directory = directory;
+        this.writers = new EnumMap<PredicateFile, Writer>(PredicateFile.class);
+
+        // Automatically generate fact files for predicates
+        // that are imported with file predicates
+        for(PredicateFile predicateFile : EnumSet.allOf(PredicateFile.class))
+        {
+            if (predicateFile.getImportScheme().equals(BY_FILE_PREDICATE))
+                writers.put(predicateFile, predicateFile.getWriter(directory, ".facts"));
+        }
+    }
+
+    @Override
+    public void close() throws IOException
+    {
+        for(Writer w: writers.values())
+            w.close();
+    }
+
+    @Override
+    public void flush() throws IOException
+    {
+        for(Writer w: writers.values())
+            w.flush();
+    }
+
+
+    protected void addColumn(Writer writer, Column column, boolean shouldTruncate)
+        throws IOException
+    {
+        // Quote some special characters
+        String data = column.toString()
+            .replaceAll("\"", "\\\\\"")
+            .replaceAll("\n", "\\\\n")
+            .replaceAll("\t", "\\\\t");
+
+        // Truncate column if necessary
+        if (shouldTruncate)
+        {
+            int length = data.length();
+
+            // TODO: log the event
+            if (length > MAX_COLUMN_LENGTH)
+                length = MAX_COLUMN_LENGTH;
+
+            writer.write(data, 0, length);
+        }
+        else {
+            writer.write(data);
+        }
+    }
+
+
+    @Override
+    public void add(PredicateFile predicateFile, Column arg, Column ... args)
+    {
+        boolean shouldTruncate = false; // predicateFile.equals(PredicateFile.STRING_CONST);
+
+        try {
+            Writer writer = getWriter(predicateFile);
+            addColumn(writer, arg, shouldTruncate);
+
+            for (Column col : args)
+                addColumn(writer.append(SEP), col, shouldTruncate);
+
+            writer.write(EOL);
+        } catch(IOException exc) {
+            throw new RuntimeException(exc);
+        }
+    }
+
+
+    protected Writer getWriter(PredicateFile predicateFile) throws IOException
+    {
+        Writer result = writers.get(predicateFile);
+
+        if (result == null)
+        {
+            result = predicateFile.getWriter(directory, ".facts");
+            writers.put(predicateFile, result);
+        }
+
+        return result;
+    }
+
+
+    @Override
+    public Column addEntity(PredicateFile predicateFile, String key)
+    {
+      add(predicateFile, new Column(key));
+      return new Column(key);
+    }
+
+
+    @Override
+    public Column asColumn(String arg) {
+        return new Column(arg);
+    }
+
+
+    @Override
+    public Column asEntity(String arg) {
+        return new Column(arg);
+    }
+
+    @Override
+    public Column asEntity(PredicateFile predicateFile, String arg) {
+        return new Column(arg);
+    }
+
+
+    @Override
+    public Column asIntColumn(String arg) {
+        return new Column(arg);
+    }
+}
