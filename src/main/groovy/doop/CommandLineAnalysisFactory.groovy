541dac511a6c7aa513803dfa7eb3f60f792e6a75
Kostas Saidis <saikos@gmail.com>
2015-03-14 17:19:13 +0200

    Support a properties file from the command line.
    A new command line option (-p, --properties) has been added, which
    loads all analysis options from the respective file. If this option
    is provided, all other options are ignored.
    
    The skeleton of a properties file has been added, too. This file is
    created by running ./gradlew createProperties.
    
    The support of additional properties files (located in $DOOP_HOME/doop.properties
    or in $HOME/doop.properties) has been droped.
    
    Finally, at the start of each analysis, a meta file is created, containing
    a dump of all its options .

--- src/main/groovy/doop/CommandLineAnalysisFactory.groovy
+++ src/main/groovy/doop/CommandLineAnalysisFactory.groovy
@@ -15,6 +15,13 @@ import org.apache.commons.cli.Option
  */
 class CommandLineAnalysisFactory extends AnalysisFactory {
 
+    private static final String LOGLEVEL = 'Set the log level: debug, info or error (default: info).'
+    private static final String ANALYSIS = 'The name of the analysis.'
+    private static final String JAR      = 'The jar files to analyze. Separate multiple jars with a comma. If the ' +
+                                           ' argument is a directory, all its *.jar files will be included.'
+    private static final String PROPS    = 'The path to a properties file containing analysis options. If this ' +
+                                           'option is given, all other options are ignored.'
+
     /**
      * Processes the cli args and generates a new analysis.
      *
@@ -29,31 +36,48 @@ class CommandLineAnalysisFactory extends AnalysisFactory {
         //Get the jars of the analysis (short option: j)
         List<String> jars = cli.js
 
-        Map<String, AnalysisOption> options = Doop.createAnalysisOptions()
+        Map<String, AnalysisOption> options = Doop.createDefaultAnalysisOptions()
+
         options.findAll { Map.Entry<String, AnalysisOption> entry ->
             entry.value.cli //get the cli options
         }.each { Map.Entry<String, AnalysisOption> entry ->
             AnalysisOption option = entry.value
             String cliOptionName = option.name
             def optionValue = cli[(cliOptionName)]
-            if (optionValue) {
+            if (optionValue) { //Only true-ish values are of interest (false or null values are ignored)
                 if (option.id == "DYNAMIC") {
                     //Obscure cli builder feature: to get the value of a cl option as a List, you need to append an s
                     //to its short name (the short name of the DYNAMIC option is d, so we invoke ds)
                     option.value = cli.ds
-                }
-                else if (option.argName) { //Only true-ish values are of interest (false or null values are ignored)
+                } else if (option.argName) {
                     //if the cl option has an arg, the value of this arg defines the value of the respective
                     // analysis option
                     option.value = optionValue
-                }
-                else {
+                } else {
                     //the cl option has no arg and thus it is a boolean flag, toggling the default value of
                     // the respective analysis option
                     option.value = !option.value
                 }
             }
         }
+        return newAnalysis(name, jars, options)
+    }
+
+    /**
+     * Processes the properties and generates a new analysis.
+     */
+    Analysis newAnalysis(Properties props) {
+
+        //Get the name of the analysis
+        String name = props.getProperty("analysis")
+
+        //Get the jars of the analysis
+        List<String> jars = props.getProperty("jar").split(",").collect { String s-> s.trim() }
+
+        Map<String, AnalysisOption> options = Doop.createDefaultAnalysisOptions()
+
+        Doop.overrideAnalysisOptionsFromProperties(options, props)
+
         return newAnalysis(name, jars, options)
     }
 
@@ -67,23 +91,100 @@ class CommandLineAnalysisFactory extends AnalysisFactory {
             option.cli //all options with cli property
         }
 
+        def list = Helper.namesOfAvailableAnalyses(Doop.doopLogic).join(', ')
+
         CliBuilder cli = new CliBuilder(
             usage:  "doop [OPTION]...",
         )
         cli.width = 120
 
         cli.with {
-            h(longOpt: 'help', 'Display help and exit')
-            l(longOpt: 'level', 'Set the log level: debug, info or error (default: debug)', args:1, argName: 'loglevel')
-            //p(longOpt: 'properties', 'Load doop properties file', args:1, argName: 'properties file')
-            a(longOpt: 'analysis', "The name of the analysis: ${Helper.namesOfAvailableAnalyses(Doop.doopLogic).join(', ')}",
-              args:1, argName:"name")
-            j(longOpt: 'jar', "The jar files to analyze. Separate multiple jars with a comma. If the argument is a directory, all its *.jar files will be included.", args:Option.UNLIMITED_VALUES, argName: "jar",
-              valueSeparator: ",")
+            h(longOpt: 'help', 'Display help and exit.')
+            l(longOpt: 'level', LOGLEVEL, args:1, argName: 'loglevel')
+            a(longOpt: 'analysis', "$ANALYSIS Allowed values: $list.", args:1, argName:"name")
+            j(longOpt: 'jar', JAR, args:Option.UNLIMITED_VALUES, argName: "jar", valueSeparator: ",")
+            p(longOpt: 'properties', PROPS, args:1, argName: "properties")
         }
 
         Helper.addAnalysisOptionsToCliBuilder(cliOptions, cli)
 
         return cli
+    }
+
+    /**
+     * Creates the default properties file containing all the supported analysis options with empty values.
+     * The file also contains the analysis name, the jars and the log level.
+     */
+    static void createEmptyProperties(File f) {
+
+        f.withWriter { Writer w ->
+
+            w.write """#
+#This is the skeleton of a doop properties file.
+#Notes:
+#- all file paths, if not absolute, should be given relative to the directory that
+#  doop is invoked from (and not relative to the directory this file is located).
+#- all booleans are processed using the java.lang.Boolean.parseBoolean() conventions.
+#- all empty properties are ignored.
+#
+
+#
+#analysis (string)
+#$ANALYSIS
+#
+analysis =
+
+#
+#jar (file)
+#$JAR
+#
+jar =
+
+#
+#level (string)
+#$LOGLEVEL
+#
+level =
+
+"""
+
+            //Find all cli options and sort them by id
+            List<AnalysisOption> cliOptions = Doop.ANALYSIS_OPTIONS.findAll { AnalysisOption option ->
+                option.cli
+            }.sort{ AnalysisOption option ->
+                option.id
+            }
+
+            //Put the "main" options first
+            cliOptions = cliOptions.findAll { AnalysisOption option -> !option.isAdvanced } +
+                    cliOptions.findAll { AnalysisOption option -> option.isAdvanced }
+
+            cliOptions.each { AnalysisOption option ->
+                writeAsProperty(option, w)
+            }
+        }
+    }
+
+    /**
+     * Writes the given analysis option to the given writer using the standard Java syntax for properties files.
+     */
+    private static void writeAsProperty(AnalysisOption option, Writer w) {
+        def type, id = option.id.toLowerCase()
+
+        if (option.isFile) {
+            type = "(file)"
+        }
+        else if (option.argName) {
+            type = "(string)"
+        }
+        else {
+            type = "(boolean)"
+        }
+
+        w.write "#\n"
+        w.write "#$id $type\n"
+        if (option.description) w.write "#${option.description} \n"
+        w.write "#\n"
+        w.write "$id = \n\n"
     }
 }
