9bc5297c63db26d2c348c60c4d614fd8683a8fd7
Yannis Smaragdakis <Yannis Smaragdakis@localhost>
2015-05-28 18:16:08 +0300

    Minor makefile changes so I can run DaCapo

---
+++ src/main/groovy/doop/input/FileResolver.groovy
@@ -0,0 +1,22 @@
+package doop.input
+
+import doop.core.Helper
+
+/**
+ * Resolves the input as a local file.
+ * @author: Kostas Saidis (saiko@di.uoa.gr)
+ * Date: 23/3/2015
+ */
+class FileResolver implements InputResolver {
+
+    @Override
+    String name() {
+        return "file"
+    }
+
+    @Override
+    void resolve(String input, InputResolutionContext ctx) {
+        File file = Helper.checkFileOrThrowException(input, "Not a file input: $input")
+        ctx.set(input, file)
+    }
+}
