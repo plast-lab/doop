41f7e5726370c18a55dd4c19e2d77391577d22d9
Kostas Saidis <saikos@gmail.com>
2015-07-18 16:08:42 +0300

    Ensure proper initialization of environment in BloxbatchConnector and minor mprovements.

--- src/main/java/doop/util/process/CalledProcessException.java
+++ src/main/java/doop/util/process/CalledProcessException.java
@@ -4,7 +4,7 @@ package doop.util.process;
  * Thrown by methods that call another process, which is required to
  * succeed (and thus expected to return a zero exit code).
  */
-class CalledProcessException extends RuntimeException
+public class CalledProcessException extends RuntimeException
 {
     private final String commandLine;
     private final int returnCode;
