c39ca4afa20cbb76c11156accc13af56d2d76c41
George Kastrinis <george.kast@gmail.com>
2015-11-03 13:57:50 +0200

    Change where analysis directories are created

--- logic/analyses/escape/wrap.sh
+++ logic/analyses/escape/wrap.sh
@@ -5,6 +5,8 @@ JAR=$1
 APPREGEX=$(jar tf $JAR | grep .class | sed -r 's/.class$//' | tr '\n' ':')
 GLOBIGNORE="lib/sootclasses-2.5.0-custom.jar"
 CLASSPATH=build/classes/main/:$(ls lib/*jar | tr '\n' ':'):$(ls ~/.gradle/wrapper/dists/gradle-2.4-bin/*/gradle-2.4/lib/*jar | tr '\n' ':')
+FACTS_DIR=out/facts
 
-rm -rf facts database
-java -cp $CLASSPATH doop.soot.Main -full -keep-line-number -use-original-names -only-application-classes-fact-gen -lsystem -application-regex $APPREGEX -allow-phantom -d facts $JAR
+rm -rf $FACTS_DIR
+java -cp $CLASSPATH doop.soot.Main -full -keep-line-number -use-original-names -only-application-classes-fact-gen -lsystem -application-regex $APPREGEX -allow-phantom -d $FACTS_DIR $JAR
+ln -s $FACTS_DIR facts
