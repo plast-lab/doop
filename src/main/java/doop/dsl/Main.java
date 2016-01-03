f077869fccc87c193ba3bb245b42a3ef7742f280
George Kastrinis <george.kast@gmail.com>
2016-01-03 20:31:00 +0200

    Add initial version of compiler

---
+++ src/main/java/doop/dsl/Main.java
@@ -0,0 +1,25 @@
+//package doop.dsl;
+
+import java.io.IOException;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			DatalogParser parser = new DatalogParser(
+										new CommonTokenStream(
+											new DatalogLexer(
+												new ANTLRFileStream(args[0]))));
+
+			DSLListener listener = new DSLListener();
+
+			ParseTreeWalker.DEFAULT.walk(listener, parser.program());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
