5d6395dee93003cf7a4d87b18be9fd111006f6e7
George Kastrinis <george.kast@gmail.com>
2016-01-10 16:55:06 +0200

    Update DSLListener to correspond to new grammar

--- src/main/java/doop/dsl/DSLListener.java
+++ src/main/java/doop/dsl/DSLListener.java
@@ -8,31 +8,41 @@ class DSLListener implements DatalogListener {
 	public void enterProgram(DatalogParser.ProgramContext ctx) {}
 	public void exitProgram(DatalogParser.ProgramContext ctx) {}
 	public void enterDeclaration(DatalogParser.DeclarationContext ctx) {
-		System.out.println("DECL: " + ctx.predicate().IDENTIFIER().getText());
+		System.out.println("DECL: " + ctx.predicate().predicateName().IDENTIFIER().getText());
 	}
 	public void exitDeclaration(DatalogParser.DeclarationContext ctx) {}
+	public void enterConstraint(DatalogParser.ConstraintContext ctx) {}
+	public void exitConstraint(DatalogParser.ConstraintContext ctx) {}
 	public void enterRule_(DatalogParser.Rule_Context ctx) {}
 	public void exitRule_(DatalogParser.Rule_Context ctx) {}
-	public void enterPredicate(DatalogParser.PredicateContext ctx) {
-		System.out.println(ctx.IDENTIFIER().getText());
-	}
+	public void enterDirective(DatalogParser.DirectiveContext ctx) {}
+	public void exitDirective(DatalogParser.DirectiveContext ctx) {}
+	public void enterPredicate(DatalogParser.PredicateContext ctx) {}
 	public void exitPredicate(DatalogParser.PredicateContext ctx) {}
 	public void enterRuleBody(DatalogParser.RuleBodyContext ctx) {}
 	public void exitRuleBody(DatalogParser.RuleBodyContext ctx) {}
+	public void enterAggregation(DatalogParser.AggregationContext ctx) {}
+	public void exitAggregation(DatalogParser.AggregationContext ctx) {}
+	public void enterRefmode(DatalogParser.RefmodeContext ctx) {}
+	public void exitRefmode(DatalogParser.RefmodeContext ctx) {}
 	public void enterFunctionalHead(DatalogParser.FunctionalHeadContext ctx) {}
 	public void exitFunctionalHead(DatalogParser.FunctionalHeadContext ctx) {}
+	public void enterPredicateName(DatalogParser.PredicateNameContext ctx) {}
+	public void exitPredicateName(DatalogParser.PredicateNameContext ctx) {}
 	public void enterPrimitiveType(DatalogParser.PrimitiveTypeContext ctx) {}
 	public void exitPrimitiveType(DatalogParser.PrimitiveTypeContext ctx) {}
+	public void enterPrimitiveConstant(DatalogParser.PrimitiveConstantContext ctx) {}
+	public void exitPrimitiveConstant(DatalogParser.PrimitiveConstantContext ctx) {}
+	public void enterParameter(DatalogParser.ParameterContext ctx) {}
+	public void exitParameter(DatalogParser.ParameterContext ctx) {}
 	public void enterComparison(DatalogParser.ComparisonContext ctx) {}
 	public void exitComparison(DatalogParser.ComparisonContext ctx) {}
 	public void enterExpr(DatalogParser.ExprContext ctx) {}
 	public void exitExpr(DatalogParser.ExprContext ctx) {}
 	public void enterPredicateList(DatalogParser.PredicateListContext ctx) {}
 	public void exitPredicateList(DatalogParser.PredicateListContext ctx) {}
-	public void enterVariable(DatalogParser.VariableContext ctx) {}
-	public void exitVariable(DatalogParser.VariableContext ctx) {}
-	public void enterVariableList(DatalogParser.VariableListContext ctx) {}
-	public void exitVariableList(DatalogParser.VariableListContext ctx) {}
+	public void enterParameterList(DatalogParser.ParameterListContext ctx) {}
+	public void exitParameterList(DatalogParser.ParameterListContext ctx) {}
 
 	public void enterEveryRule(ParserRuleContext ctx) {}
 	public void exitEveryRule(ParserRuleContext ctx) {}
