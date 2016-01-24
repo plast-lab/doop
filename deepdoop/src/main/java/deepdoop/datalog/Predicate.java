eedcad5d1e94015d7ce43b5325b2b4d3b4d7a6b0
George Kastrinis <george.kast@gmail.com>
2016-01-24 16:33:47 +0200

    Add support for functional predicates

--- deepdoop/src/main/java/deepdoop/datalog/Predicate.java
+++ deepdoop/src/main/java/deepdoop/datalog/Predicate.java
@@ -1,18 +1,33 @@
 package deepdoop.datalog;
 
 import java.util.List;
+import java.util.StringJoiner;
 
 public class Predicate {
 	String _name;
 	List<String> _types;
 
+	public final boolean isFunctional;
+
+
 	public Predicate(String name, List<String> types) {
+		this(name, types, false);
+	}
+	public Predicate(String name, List<String> types, boolean isFunctional) {
 		_name = name;
 		_types = types;
+		this.isFunctional = isFunctional;
 	}
 
 	@Override
 	public String toString() {
-		return _name + "/" + _types.size() + " (" + DatalogListenerImpl.join(_types, " x ") + ")";
+		StringJoiner joiner = new StringJoiner(" x ");
+		for (String s : _types) joiner.add(s);
+		String t = joiner.toString();
+		if (isFunctional) {
+			int pos = t.lastIndexOf(" x ");
+			t = new StringBuilder(t).replace(pos, pos+3, " -> ").toString();
+		}
+		return _name + "/" + _types.size() + " (" + t + ")";
 	}
 }
