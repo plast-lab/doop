1360e84a9c48a5b9553f2d9a458620544a9f82d1
George Kastrinis <george.kast@gmail.com>
2016-01-27 05:36:38 +0200

    Add handling of various IElement types

---
+++ deepdoop/src/main/java/deepdoop/datalog/ExprElement.java
@@ -0,0 +1,60 @@
+package deepdoop.datalog;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class ExprElement implements IElement {
+
+	public enum Operator {
+		PLUS("+"), MINUS("-"), MULT("*"), DIV("/");
+
+		private String _op;
+
+		Operator(String op) {
+			_op = op;
+		}
+
+		@Override
+		public String toString() {
+			return _op;
+		}
+	}
+
+	ExprElement _left;
+	Operator _op;
+	ExprElement _right;
+
+	//ExprElement _expr;
+
+	Variable _var;
+
+	PredicateInstance _funcHead;
+
+	String _constant;
+
+
+	public ExprElement(ExprElement left, Operator op, ExprElement right) {
+		_left = left;
+		_op = op;
+		_right = right;
+	}
+	public ExprElement(Variable var) {
+	   _var = var;
+	}
+	public ExprElement(PredicateInstance funcHead) {
+		_funcHead = funcHead;
+	}
+	public ExprElement(String constant) {
+		_constant = constant;
+	}
+
+	@Override
+	public String toString() {
+		if (_op != null) return _left + " " + _op + " " + _right;
+		else if (_var != null) return _var.toString();
+		else if (_funcHead != null) return _funcHead.toString();
+		else if (_constant != null) return _constant;
+		else return "Expr";
+	}
+}
