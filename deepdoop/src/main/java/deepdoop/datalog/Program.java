077bc7e1c2e86f97dd0455251dc39244bd58e065
George Kastrinis <george.kast@gmail.com>
2016-02-02 14:26:10 +0200

    Add initial version of Program

---
+++ deepdoop/src/main/java/deepdoop/datalog/Program.java
@@ -0,0 +1,25 @@
+package deepdoop.datalog;
+
+import java.util.Set;
+
+public class Program {
+	Set<Predicate> _predicates;
+	Set<Predicate> _specialPredicates;
+	Set<Rule> _rules;
+
+	public Program(Set<Predicate> predicates, Set<Predicate> specialPredicates, Set<Rule> rules) {
+		_predicates = predicates;
+		_specialPredicates = specialPredicates;
+		_rules = rules;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		for (Predicate p : _predicates) builder.append(p + "\n");
+		for (Predicate p : _specialPredicates) builder.append(p + "\n");
+		builder.append("\n");
+		for (Rule r : _rules) builder.append(r + "\n");
+		return builder.toString();
+	}
+}
