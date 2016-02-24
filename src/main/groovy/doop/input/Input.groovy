d652ac40a8404e2aaf3be6416fd4d720f69a28e3
George Kastrinis <george.kast@gmail.com>
2016-02-24 15:59:45 +0200

    Remove dexlib and util jars

---
+++ src/main/groovy/doop/input/Input.groovy
@@ -0,0 +1,12 @@
+package doop.input
+
+/**
+ * The input to an analysis: a string that may correspond to one or more files.
+ *
+ * @author: Kostas Saidis (saiko@di.uoa.gr)
+ * Date: 23/3/2015
+ */
+interface Input {
+    String name();
+    Set<File> files();
+}
