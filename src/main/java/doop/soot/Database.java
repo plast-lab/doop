1da8ca11197482a2c8dea762372ee8c16c9a9927
Neville Grech <nevillegrech@gmail.com>
2016-04-05 12:32:40 +0300

    Initial taint analysis commit
    Add relations to specify:
     TaintedHeapAllocation
     TaintedInformationFlowsTo
    
    TBC

---
+++ src/main/java/doop/soot/Database.java
@@ -0,0 +1,15 @@
+package doop.soot;
+
+import java.io.IOException;
+import java.io.Closeable;
+import java.io.Flushable;
+
+public interface Database extends Closeable, Flushable
+{
+    public void add(PredicateFile predFile, Column arg, Column... args);
+    public Column addEntity(PredicateFile predFile, String key);
+    public Column asColumn(String arg);
+    public Column asIntColumn(String arg);
+    public Column asEntity(String arg);
+    public Column asEntity(PredicateFile predFile, String arg);
+}
