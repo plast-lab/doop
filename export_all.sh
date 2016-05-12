6039f5b938dc29c1781b7979a3ec1d673aff6b6e
Yannis Smaragdakis <Yannis Smaragdakis@localhost>
2016-05-13 01:54:12 +0300

    intermediate commit, in the middle of forall emulations

--- export_all.sh
+++ export_all.sh
@@ -3,17 +3,20 @@
 AssignCast:Insn AssignContextInsensitiveHeapAllocation \
 AssignHeapAllocation AssignLocal:From AssignLocal:Insn  \
 AssignNormalHeapAllocation AssignInstruction:To AssignNull:Insn \
-AssignReturnValue FieldInstruction:Signature FieldModifier \
+AssignReturnValue \
+BasicBlockBegin BasicBlockHead BasicBlockEnd \
+ExistsPreviousReturn FieldInstruction:Signature FieldModifier \
 FieldSignature:DeclaringClass FieldSignature:Type FormalParam HeapAllocation:Merge \
 HeapAllocation:Null \
 HeapAllocation:Type Instruction:Index Instruction:Method IsJumpTarget \
 LoadInstanceField:Base LoadInstanceField:To LoadStaticField:To \ 			
 MainClass MainMethodArgHeap  \
-MainMethodArgsArray MethodInvocation:Signature MethodLookup \
+MainMethodArgsArray MaySuccessorBBModuloThrow \
+MethodInvocation:Signature MethodLookup \
 MethodSignature:DeclaringType MethodSignature:Descriptor \
 MethodSignature:SimpleName Modifier:final \
-NextInSamePhiNode PhiNodeHead Reachable \
-ReferenceType ReturnVar SpecialMethodInvocation:Base \
+NextInSamePhiNode NextReturn PhiNodeHead Reachable \
+ReferenceType ReturnInstruction ReturnNonvoid:Var ReturnVar SpecialMethodInvocation:Base \
 SpecialMethodInvocation:Insn StaticMethodInvocation:Insn \
 StoreInstanceField:Base StoreInstanceField:From StoreStaticField:From \ 			
 SubtypeOf ThisVar \
