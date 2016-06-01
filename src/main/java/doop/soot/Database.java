1a8211658a1c2c73f8d7cac786f7d24a423295d4
Anastasios Antoniadis <anantoni@di.uoa.gr>
2016-06-01 21:26:18 +0300

    Hack in soot-fact-generation to overcome the Var:Type problem when enabling use-original-names

---
+++ src/main/java/doop/soot/Database.java
@@ -0,0 +1,14 @@
+package doop.soot;
+
+import java.io.Closeable;
+import java.io.Flushable;
+
+interface Database extends Closeable, Flushable
+{
+    void add(PredicateFile predFile, Column arg, Column... args);
+    Column addEntity(PredicateFile predFile, String key);
+    Column asColumn(String arg);
+    Column asIntColumn(String arg);
+    Column asEntity(String arg);
+    Column asEntity(PredicateFile predFile, String arg);
+}
