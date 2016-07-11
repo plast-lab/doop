eef1efc1d6eac8e49d13f29b6c4e970f5b6c7d1c
Yannis Smaragdakis <Yannis Smaragdakis@localhost>
2016-07-11 21:32:12 +0300

    add control-flow merge point rules

--- export_all.sh
+++ export_all.sh
@@ -4,18 +4,19 @@ AssignCast:Insn AssignContextInsensitiveHeapAllocation \
 AssignHeapAllocation AssignLocal:From AssignLocal:Insn  \
 AssignNormalHeapAllocation AssignInstruction:To AssignNull:Insn \
 AssignReturnValue \
-BasicBlockBegin BasicBlockHead BasicBlockEnd \
-ExistsPreviousReturn FieldInstruction:Signature FieldModifier \
+BasicBlockBegin BasicBlockHead BasicBlockEnd ExceptionHandlerFirstInstruction \
+ExistsPreviousPredecessorToSameBB ExistsPreviousReturn \
+FieldInstruction:Signature FieldModifier \
 FieldSignature:DeclaringClass FieldSignature:Type FormalParam HeapAllocation:Merge \
 HeapAllocation:Null \
 HeapAllocation:Type Instruction:Index Instruction:Method IsJumpTarget \
 LoadInstanceField:Base LoadInstanceField:To LoadStaticField:To \ 			
 MainClass MainMethodArgHeap  \
-MainMethodArgsArray MaySuccessorBBModuloThrow \
+MainMethodArgsArray MayPredecessorBBModuloThrow MaySuccessorBBModuloThrow \
 MethodInvocation MethodInvocation:Signature MethodLookup \
 MethodSignature:DeclaringType MethodSignature:Descriptor \
-MethodSignature:SimpleName Modifier:final MonitorInstruction \
-NextInSamePhiNode NextReturn PhiNodeHead PrevInSameBasicBlock Reachable \
+MethodSignature:SimpleName Modifier:final Modifier:static MonitorInstruction \
+NextInSamePhiNode NextPredecessorToSameBB NextReturn PhiNodeHead PrevInSameBasicBlock Reachable \
 ReferenceType ReturnInstruction ReturnNonvoid:Var ReturnVar SpecialMethodInvocation:Base \
 SpecialMethodInvocation:Insn StaticMethodInvocation:Insn \
 StoreInstanceField:Base StoreInstanceField:From StoreStaticField:From \ 			
