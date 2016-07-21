9f3d281e20a10b07dc7fbd5a4a323596cd97a09c
Anastasios Antoniadis <anastasios.antoniadis@gmail.com>
2016-07-21 18:16:10 +0300

    Changing predicate capacities

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-./doop -j ~/android/com.facebook.katana_v86.0.0.13.69-33840476_Android-5.0.apk -a context-insensitive --platform android_23 -ldebug
+#!/usr/bin/env bash
+./doop -j ~/android/com.facebook.katana_v86.0.0.13.69-33840476_Android-5.0.apk -a context-insensitive --platform android_23 --cache -ldebug
