bcd9610521332afa508761136b17706f6b327319
Yannis Smaragdakis <Yannis Smaragdakis@localhost>
2016-07-25 15:40:59 +0300

    merged and committed my run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+<<<<<<< local
+./doop -j /usr/data/anantoni/Android-apks/com.facebook.katana_v86.0.0.13.69-33840476_Android-5.0.apk -a context-insensitive $* --timeout 600 --platform android_23 --cache -ldebug 
+=======
 ./doop -j ~/android/com.facebook.katana_v86.0.0.13.69-33840476_Android-5.0.apk -a context-insensitive  --enable-reflection --enable-reflection-string-flow-analysis --enable-reflection-substring-analysis --enable-reflection-invent-unknown-objects --platform android_23 --timeout 600 --cache -ldebug
+>>>>>>> other
