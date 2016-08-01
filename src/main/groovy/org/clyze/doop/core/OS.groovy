0581b24ea9538e1bd4d0fe317e80ee97a048cc24
Neville Grech <nevillegrech@gmail.com>
2016-08-02 00:00:36 +0300

    Added sources and sinks to improve recall

---
+++ src/main/groovy/org/clyze/doop/core/OS.groovy
@@ -0,0 +1,11 @@
+package org.clyze.doop.core
+
+/**
+ * The OS values.
+ * Currently only the OS_UNIX value is supported (it is used by the logic files).
+ */
+enum OS {
+    OS_UNIX,
+    OS_WINNT,
+    OS_WIN32
+}
---
+++ src/main/groovy/org/clyze/doop/core/PreprocessorFlag.groovy
@@ -0,0 +1,9 @@
+package org.clyze.doop.core
+/**
+ * The types of preprocessor flags
+ */
+ enum PreprocessorFlag {
+	NORMAL_FLAG,
+	EXCEPTION_FLAG,
+	CONSTANT_FLAG
+ }
