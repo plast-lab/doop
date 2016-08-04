54e33aff751b29dad6bcd2e3a4893ceb209ff824
Yannis Smaragdakis <Yannis Smaragdakis@localhost>
2016-08-04 17:54:23 +0300

    parameterize SoundMayPointTo component, so that different phases can use different MustPointTo

---
+++ src/main/groovy/org/clyze/doop/core/AnalysisPostProcessor.groovy
@@ -0,0 +1,8 @@
+package org.clyze.doop.core
+
+/**
+ * Created by saiko on 19/10/2015.
+ */
+interface AnalysisPostProcessor {
+    void process(Analysis analysis)
+}
---
+++ src/main/groovy/org/clyze/doop/input/DirectoryResolver.groovy
@@ -0,0 +1,28 @@
+package org.clyze.doop.input
+import org.clyze.doop.core.Helper
+/**
+ * Resolves the input as a directory that contains jar files.
+ * @author: Kostas Saidis (saiko@di.uoa.gr)
+ * Date: 23/3/2015
+ */
+class DirectoryResolver implements InputResolver {
+
+    @Override
+    String name() {
+        return "directory"
+    }
+
+    @Override
+    void resolve(String input, InputResolutionContext ctx) {
+        File f = Helper.checkDirectoryOrThrowException(input, "Not a directory input: $input")
+
+        def filter = Helper.extensionFilter("jar")
+
+        List<File> filesInDir = []
+        f.listFiles(filter).each { File file ->
+            filesInDir.add(file)
+        }
+
+        ctx.set(input, filesInDir.sort{ it.toString() });
+    }
+}
