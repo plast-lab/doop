bca40aa3552c3bebcae982bf9cffa93c52289b7c
Neville Grech <nevillegrech@gmail.com>
2016-09-30 21:57:54 +0200

    IBD/EBD HeapAllocation refactor for information-flow addon

---
+++ src/main/groovy/org/clyze/doop/input/FileResolver.groovy
@@ -0,0 +1,20 @@
+package org.clyze.doop.input
+
+import org.clyze.doop.system.FileOps
+
+/**
+ * Resolves the input as a local file.
+ */
+class FileResolver implements InputResolver {
+
+    @Override
+    String name() {
+        return "file"
+    }
+
+    @Override
+    void resolve(String input, InputResolutionContext ctx) {
+        def file = FileOps.findFileOrThrow(input, "Not a file input: $input")
+        ctx.set(input, file)
+    }
+}
