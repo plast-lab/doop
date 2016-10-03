f2d5fa5cd334f1276f263d14d200692a579bd3f4
George Kastrinis <george.kast@gmail.com>
2016-10-03 16:26:51 +0300

    Integrate DeepDoop code with the main codebase (under src)

--- deepdoop/src/main/java/deepdoop/datalog/element/IElement.java
+++
@@ -1,5 +0,0 @@
-package deepdoop.datalog.element;
-
-import deepdoop.actions.IVisitable;
-
-public interface IElement extends IVisitable {}
---
+++ src/main/java/org/clyze/deepdoop/actions/IVisitor.java
@@ -0,0 +1,39 @@
+package org.clyze.deepdoop.actions;
+
+import org.clyze.deepdoop.datalog.*;
+import org.clyze.deepdoop.datalog.clause.*;
+import org.clyze.deepdoop.datalog.component.*;
+import org.clyze.deepdoop.datalog.element.*;
+import org.clyze.deepdoop.datalog.element.atom.*;
+import org.clyze.deepdoop.datalog.expr.*;
+
+public interface IVisitor<T> {
+	T visit(Program n);
+
+	T visit(CmdComponent n);
+	T visit(Component n);
+
+	T visit(Constraint n);
+	T visit(Declaration n);
+	T visit(RefModeDeclaration n);
+	T visit(Rule n);
+
+	T visit(AggregationElement n);
+	T visit(ComparisonElement n);
+	T visit(GroupElement n);
+	T visit(LogicalElement n);
+	T visit(NegationElement n);
+
+	T visit(Directive n);
+	T visit(Functional n);
+	T visit(Predicate n);
+	T visit(Primitive n);
+	T visit(RefMode n);
+	T visit(StubAtom n);
+
+	T visit(BinaryExpr n);
+	T visit(ConstantExpr n);
+	T visit(FunctionalHeadExpr n);
+	T visit(GroupExpr n);
+	T visit(VariableExpr n);
+}
