34cabf7b946d95cdbc115b12f7c8dd0c270d75cf
George Kastrinis <george.kast@gmail.com>
2016-10-07 15:26:18 +0300

    Fix Example.java

--- docs/doop-101-examples/Example.java
+++ docs/doop-101-examples/Example.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Example
 {
 	public static void main(String[] args) throws IOException{
