38068673897447b3522d8b216fd1dd1cb01dc4f0
Neville Grech <nevillegrech@gmail.com>
2016-10-09 17:19:34 +0200

    Experiment with a bottom type for information heap allocations Experiment removing the dependency on the context sensitive callgraph

---
+++ src/main/groovy/org/clyze/doop/blox/BloxbatchScript.groovy
@@ -0,0 +1,78 @@
+package org.clyze.doop.blox
+
+import org.apache.commons.io.FileUtils
+import org.clyze.doop.system.Executor
+
+class BloxbatchScript {
+
+    File script
+    Writer writer;
+
+    public BloxbatchScript(File script) {
+        this.script = script
+        writer = new PrintWriter(script)
+    }
+
+    public String getPath() {
+        return script.getPath()
+    }
+
+    public void close() {
+        writer.close()
+    }
+
+    public BloxbatchScript echo(String message) {
+        return wr('echo "' + message + '"')
+    }
+    public BloxbatchScript startTimer() {
+        return wr("startTimer")
+    }
+    public BloxbatchScript elapsedTime() {
+        return wr("elapsedTime")
+    }
+    public BloxbatchScript transaction() {
+        return wr("transaction")
+    }
+    public BloxbatchScript commit() {
+        return wr("commit")
+    }
+    public BloxbatchScript createDB(String database) {
+        return wr("create $database --overwrite --blocks base")
+    }
+    public BloxbatchScript addBlock(String logiqlString) {
+        return wr("addBlock '$logiqlString'")
+    }
+    public BloxbatchScript addBlockFile(String filePath) {
+        return wr("addBlock -F $filePath")
+    }
+    public BloxbatchScript addBlockFile(String filePath, String blockName) {
+        return wr("addBlock -F $filePath -B $blockName")
+    }
+    public BloxbatchScript execute(String logiqlString) {
+        return wr("exec '$logiqlString'")
+    }
+    public BloxbatchScript executeFile(String filePath) {
+        return wr("exec -F $filePath")
+    }
+    public BloxbatchScript wr(String message) {
+        writer.println(message)
+        return this
+    }
+
+    public BloxbatchScript include(String filePath) {
+        def inDir  = (new File(filePath)).getParentFile()
+        def outDir = script.getParentFile()
+        def file   = new File(outDir, "_tmp.logic")
+        new Executor(System.getenv()).execute("cpp -P $filePath $file")
+        file.eachLine { line ->
+            def matcher = (line =~ /^(addBlock|exec)[ \t]+-[a-zA-Z][ \t]+(.*\.logic)$/)
+            if (matcher.matches()) {
+                def inFile  = matcher[0][2]
+                def outFile = inFile.replaceAll(File.separator, "-")
+                FileUtils.copyFile(new File(inDir, inFile), new File(outDir, outFile))
+            }
+            writer.println(line)
+        }
+        return this
+    }
+}
---
+++ src/main/java/org/clyze/deepdoop/datalog/expr/IExpr.java
@@ -0,0 +1,5 @@
+package org.clyze.deepdoop.datalog.expr;
+
+import org.clyze.deepdoop.actions.IVisitable;
+
+public interface IExpr extends IVisitable {}
