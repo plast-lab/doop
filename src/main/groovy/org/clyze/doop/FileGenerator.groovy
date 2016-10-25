a2a4a8bcb6fa2ac00d461aabca4e49b4a2bc2536
Kostas Saidis <saikos@gmail.com>
2016-10-25 13:28:57 +0300

    Making doop a binary dependency of the server (an additional jar that contains the logic files is published). Removal of pre-calculation of any checksums (no more checksums.properties).

--- src/main/groovy/org/clyze/doop/FileGenerator.groovy
+++ src/main/groovy/org/clyze/doop/FileGenerator.groovy
@@ -9,8 +9,7 @@ import org.clyze.doop.system.*
 class FileGenerator {
 
     /**
-     * Accepts three arguments: (a) the directory to write files to, (b) the path of soot-classes jar (c) the path of
-     * jphantom jar.
+     * Accepts a single argument, the directory to write files to.
      */
     static void main(String[] args) {
         if (args && args.length == 2) {
@@ -18,7 +17,8 @@ class FileGenerator {
                 def dir = FileOps.findDirOrThrow(args[0], "Invalid directory: ${args[0]}")
                 def properties = new File(dir, "doop.properties")
                 CommandLineAnalysisFactory.createEmptyProperties(properties)
-
+                /*
+                //Don't pre-calculate any checksums
                 def checksums = new File(dir, "checksums.properties")
                 def sootClassesJar = FileOps.findFileOrThrow(args[1], "Invalid soot classes jar: ${args[1]}")
                 def alg = "SHA-256"
@@ -28,6 +28,7 @@ class FileGenerator {
                             ${Doop.SOOT_CHECKSUM_KEY} = ${CheckSum.checksum(sootClassesJar, alg)}
                             """.stripIndent()
                 }
+                */
             } catch (e) {
                 println e.getMessage()
                 System.exit(-1)
