6c9ccc08385219e79db53afd299032574772787e
Yannis Smaragdakis <Yannis Smaragdakis@localhost>
2016-10-28 01:47:30 +0300

    make simple must-point-to work by aliasing formals/actuals at calls

---
+++ export_all.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+./export.sh ActualParam ApplicationClass ArrayType AssignCast:From \
+AssignCast:Insn AssignContextInsensitiveHeapAllocation \
+AssignHeapAllocation AssignLocal:From AssignLocal:Insn  \
+AssignNormalHeapAllocation AssignInstruction:To AssignNull:Insn \
+AssignReturnValue \
+BasicBlockBegin BasicBlockHead BasicBlockEnd ExceptionHandlerFirstInstruction \
+ExistsPreviousPredecessorToSameBB ExistsPreviousReturn \
+FieldInstruction:Signature FieldModifier \
+FieldSignature:DeclaringClass FieldSignature:Type FormalParam HeapAllocation:Merge \
+HeapAllocation:Null \
+HeapAllocation:Type Instruction:Index Instruction:Method IsJumpTarget \
+LoadArrayIndex:Base LoadArrayIndex:To LoadInstanceField:Base LoadInstanceField:To LoadStaticField:To \ 			
+MainClass MainMethodArgHeap  \
+MainMethodArgsArray MayPredecessorBBModuloThrow MaySuccessorBBModuloThrow \
+MethodInvocation MethodInvocation:Signature MethodLookup \
+MethodSignature:DeclaringType MethodSignature:Descriptor \
+MethodSignature:SimpleName Modifier:final Modifier:static MonitorInstruction \
+NextInSamePhiNode NextPredecessorToSameBB NextReturn PhiNodeHead PrevInSameBasicBlock Reachable \
+ReferenceType ReturnInstruction ReturnNonvoid:Var ReturnVar SpecialMethodInvocation:Base \
+SpecialMethodInvocation:Insn StaticMethodInvocation:Insn \
+StoreArrayIndex:Base StoreArrayIndex:From \
+StoreInstanceField:Base StoreInstanceField:From StoreStaticField:From \ 			
+SubtypeOf ThisVar \
+Var:DeclaringMethod Var:Type VirtualMethodInvocation:Base \
+VirtualMethodInvocation:Descriptor VirtualMethodInvocation:SimpleName 
+#./export2.sh 1 Reachable
+#./export2.sh 2 VirtualMethodInvocation:SimpleName
+#./export2.sh 2 VirtualMethodInvocation:Descriptor
+#./export2.sh 2 SubtypeOf
+#./export2.sh 2 HeapAllocation:Merge
+#./export2.sh 2 PhiNodeHead
+#./export2.sh 2 NextInSamePhiNode
+#./export2.sh 3 AssignContextInsensitiveHeapAllocation
+#./export2.sh 3 AssignNormalHeapAllocation
+#./export2.sh 4 MethodLookup
+
+
