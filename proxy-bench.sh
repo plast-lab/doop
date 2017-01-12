eecc8dc8e6fd8db023cfb52b915c384f76c212b8
George Fourtounis <gfour@di.uoa.gr>
2017-01-12 22:06:45 +0200

    Update guice-jndi bench.

--- proxy-bench.sh
+++ proxy-bench.sh
@@ -73,7 +73,7 @@ do
 	    ;;
 	guice-jndi)
 	    # Google Guice JNDI test.
-	    runDoopFor "${GDIR}/core/target/guice-4.1.0-tests.jar ${GDIR}/core/target/guice-4.1.0-jar-with-dependencies.jar ${GDIR}/libs" "--main com.google.inject.example.JndiProviderClient"
+	    runDoopFor "${GDIR}/guice-4.1.0-tests.jar ${GDIR}/libs" "--main com.google.inject.example.JndiProviderClient"
 	    ;;
 	jconsole)
 	    # OpenJDK benchmark: JConsole.
