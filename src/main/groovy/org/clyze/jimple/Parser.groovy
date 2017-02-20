c57aadd41d6cb8c7d42c6fbc4df22ddddc0a9f2c
Kostas Saidis <saikos@gmail.com>
2017-02-21 01:28:53 +0200

    Changes in jimple parsing to use a common machinery

--- src/main/groovy/org/clyze/jimple/Parser.groovy
+++ src/main/groovy/org/clyze/jimple/Parser.groovy
@@ -6,6 +6,8 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker
 import org.apache.commons.io.FileUtils
 import org.apache.commons.io.FilenameUtils
 
+import org.clyze.persistent.doop.BasicMetadata
+
 class Parser {
 
 	static void parse(String filename) {
@@ -43,5 +45,48 @@ class Parser {
 
 		// XYZ/abc.def.Foo.json
 		new File(dir, simplename + ".json").withWriter { it << listener.json }
+	}
+
+    static BasicMetadata parseJimple(String filename) {
+    	/* 
+
+    	Are these needed?
+
+		// XYZ/abc.def.Foo.jimple
+		def origFile = new File(filename)
+		// XYZ
+		def dir = origFile.getParentFile()
+		dir = dir ?: new File(".")
+		// abc.def.Foo
+		def simplename = FilenameUtils.removeExtension( FilenameUtils.getName(filename) )
+		def i = simplename.lastIndexOf(".")
+		// abc.def
+		def packages = simplename[0..i]
+		// Foo
+		def classname = simplename[(i+1)..-1]
+		File sourceFile
+		if (i != -1) {
+			// XYZ/abc/def
+			def path = new File(dir, packages.replaceAll("\\.", "/"))
+			path.mkdirs()
+			// XYZ/abc/def/Foo.jimple
+			sourceFile = new File(path, classname + ".jimple")
+			FileUtils.copyFile(origFile, sourceFile)
+		}
+		else
+			sourceFile = origFile
+		*/
+
+		File sourceFile = new File(filename) //does the sourceFileName gets reported in the same way with the jcplugin?
+
+
+		JimpleParser parser = new JimpleParser(
+				new CommonTokenStream(
+					new JimpleLexer(
+						new ANTLRFileStream(sourceFile as String))))
+		JimpleListenerImpl listener = new JimpleListenerImpl(sourceFile as String)
+		ParseTreeWalker.DEFAULT.walk(listener, parser.program())
+
+        return listener.metadata
 	}
 }
