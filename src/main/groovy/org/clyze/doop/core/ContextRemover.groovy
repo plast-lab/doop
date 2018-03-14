3445bd2a0eb7bef07ac4e3c7103bf7ee8f4d4ebf
George Fourtounis <gfour@di.uoa.gr>
2018-03-14 13:33:37 +0200

    Minor edit.

--- src/main/groovy/org/clyze/doop/core/ContextRemover.groovy
+++ src/main/groovy/org/clyze/doop/core/ContextRemover.groovy
@@ -37,7 +37,7 @@ public class ContextRemover {
         println "WARNING: Running the experimental context remover..."
         outFile.delete()
 
-        List outLines = ['.decl TRUE(t:number)', 'TRUE(1).']
+        List outLines = ['.decl TRUE(t:number)', '${CONST_TRUE}.']
         // First pass: recognize and transform declarations.
         inFile.eachLine { String line -> outLines << translateDecl(line) }
         // Second pass: transform logic and remove most trivial placeholders.
