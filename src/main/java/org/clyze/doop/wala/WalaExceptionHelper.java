fb2440feceb958ce8dbad207435ee3359f67ebb3
Sifis Lagouvardos <sifislag@gmail.com>
2018-05-08 20:50:10 +0300

    WALA: Started to rewrite exception handling as there are still cases with wrong exception handler ranges. Specifically cases where we produce [A,D] but it should be [A,B],[C,D] and not catch exceptions between B and C.

---
+++ src/main/java/org/clyze/doop/wala/WalaExceptionHelper.java
@@ -0,0 +1,116 @@
+package org.clyze.doop.wala;
+
+import com.ibm.wala.shrikeBT.ExceptionHandler;
+import com.ibm.wala.ssa.SSAInstruction;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WalaExceptionHelper {
+    int[][] exceArrays;
+    String[][] exceTypeArrays;
+    SSAInstruction[] instructions;
+    Map<Integer,Integer[]> walaHandlerScopes;
+    ExceptionHandler[][] shrikeExceptionHandlers;
+
+    public WalaExceptionHelper(SSAInstruction[] instrs, ExceptionHandler[][] shrikeExcHandlers)
+    {
+        shrikeExceptionHandlers = shrikeExcHandlers;
+        instructions = instrs;
+        exceArrays = new int[instructions.length][];
+        exceTypeArrays = new String[instructions.length][];
+        walaHandlerScopes = new HashMap<>();
+    }
+
+    public int getWalaExceptionID(int instrIndex, int exceptionIndex)
+    {
+        return exceArrays[instrIndex][exceptionIndex];
+    }
+
+    public int[] getWalaExceptionIDArray(int instrIndex)
+    {
+        return exceArrays[instrIndex];
+    }
+
+    public String getWalaExceptionType(int instrIndex, int exceptionIndex)
+    {
+        return exceTypeArrays[instrIndex][exceptionIndex];
+    }
+
+    public String[] getWalaExceptionTypeArray(int instrIndex)
+    {
+        return exceTypeArrays[instrIndex];
+    }
+
+    public void computeArraysForInstruction(int instrIndex)
+    {
+        if(instructions[instrIndex] != null)
+        {
+            exceArrays[instrIndex] = handlersToArray(shrikeExceptionHandlers[instrIndex]);
+            exceTypeArrays[instrIndex] = handlersStringArray(shrikeExceptionHandlers[instrIndex]);
+        }
+        else
+        {
+            exceArrays[instrIndex] = null;
+            exceTypeArrays[instrIndex] = null;
+        }
+    }
+
+    public Integer[] computeScopeForExceptionHandler(int walaExceptionHandlerID)
+    {
+        if(walaHandlerScopes.containsKey(walaExceptionHandlerID))
+            return walaHandlerScopes.get(walaExceptionHandlerID);
+
+        int scopeStart = -1;
+        boolean previous = false;
+        boolean found;
+        ArrayList<Integer> scopeArrayList = new ArrayList<>();
+        for(int i=0; i < exceArrays.length;i++)
+        {
+            found = false;
+            if(exceArrays[i] == null)
+                continue;
+            for(int j=0; j < exceArrays[i].length; j++) {
+                if (walaExceptionHandlerID == exceArrays[i][j]) {
+                    if (scopeStart == -1)
+                        scopeStart = i;
+                    found = true;
+                    break;
+                }
+            }
+            if(!found && previous)
+            {
+                scopeArrayList.add(scopeStart);
+                scopeArrayList.add(i);
+                scopeStart = -1;
+            }
+            previous = found;
+        }
+        Integer[] scopeArray = new Integer[scopeArrayList.size()];
+        scopeArray = scopeArrayList.toArray(scopeArray);
+        walaHandlerScopes.put(walaExceptionHandlerID,scopeArray);
+        return scopeArray;
+    }
+
+    int[] handlersToArray(ExceptionHandler[] handlers)
+    {
+        int[] targetsArray = new int[handlers.length];
+        for(int i=0; i < handlers.length ; i++)
+        {
+            targetsArray[i] = handlers[i].getHandler();
+        }
+        return targetsArray;
+    }
+
+    String[] handlersStringArray(ExceptionHandler[] handlers)
+    {
+        String[] targetsArray = new String[handlers.length];
+        for(int i=0; i < handlers.length ; i++)
+        {
+            targetsArray[i] = handlers[i].getCatchClass();
+        }
+        return targetsArray;
+    }
+
+}
