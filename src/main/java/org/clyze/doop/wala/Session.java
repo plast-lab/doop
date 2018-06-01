a1e682dd9e2b979ee327d7fc8d766b42c17259c6
Sifis Lagouvardos <sifislag@gmail.com>
2018-06-01 15:31:48 +0300

    WALA: Documenting our use of Session as it is different than the Soot front end.

--- src/main/java/org/clyze/doop/wala/Session.java
+++ src/main/java/org/clyze/doop/wala/Session.java
@@ -24,17 +24,31 @@ public class Session
         return x;
     }
 
-    /** keeps the unique index of an instruction in the method. This cannot be computed up front,
-     because temporary variables (and assignments to them from constants) will be inserted
-     while the Jimple code is being processed. */
+    /*
+     * We keep 2 indexes for each instruction
+     * We do this because assign constant instructions do not exist in WALA and constants are used directly as parameters
+     * When an instruction uses one or more constants we create fake assign constant instructions in WalaFactGenerator::generateUses()
+     * We use the instruction itself to create the index/instructionNumber of the fake instructions. After the fake assign
+     * instructions have been generated, the call to calcInstructionNumber() when generating instruction-specific facts
+     * creates the actual index of the instruction. Because this is the last call to calcInstructionNumber() this index
+     * will be in _instructionsMaxIndex.
+     * When looking for the index of an instruction in most cases we need the "first" index that is stored in _instructions
+     * In some cases (ex. when creating ranges of exception handlers) you may need the "last" index.
+     *
+     * In general it is very important to understand how to correctly use Session when applying changes to the WALA front end
+     * Unnecessary calls to calcInstructionNumber() (in contrast with the SOOT front end where they do not affect the facts) can create wrong facts.
+     */
     private Map<SSAInstruction, Integer> _instructions = new HashMap<>();
     private Map<SSAInstruction, Integer> _instructionsMaxIndex = new HashMap<>();
     private int index = 0;
 
+    /**
+     WARNING: it is important to know when this method should be used instead of getInstructionNumber() or getMaxInstructionNumber()
+     Check the detailed comment lines above this method.
+     */
     public int calcInstructionNumber(SSAInstruction instruction)
     {
         index++;
-
         // record the first unit number for this units (to handle jumps)
         _instructions.putIfAbsent(instruction, index);
         _instructionsMaxIndex.put(instruction, index);
