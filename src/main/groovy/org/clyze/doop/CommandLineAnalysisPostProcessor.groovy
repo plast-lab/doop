dd07e8a295e2dac20f0917f9bddf13694cf14e4b
George Kastrinis <george.kast@gmail.com>
2018-06-19 00:57:40 +0300

    Clean-up CommandLineAnalysisPostProcessor

--- src/main/groovy/org/clyze/doop/CommandLineAnalysisPostProcessor.groovy
+++ src/main/groovy/org/clyze/doop/CommandLineAnalysisPostProcessor.groovy
@@ -3,101 +3,101 @@ package org.clyze.doop
 import org.apache.commons.io.FilenameUtils
 import org.apache.commons.logging.Log
 import org.apache.commons.logging.LogFactory
-import org.clyze.analysis.Analysis
 import org.clyze.analysis.AnalysisPostProcessor
 import org.clyze.doop.core.Doop
 import org.clyze.doop.core.DoopAnalysis
 
+import java.nio.file.Files
+
 class CommandLineAnalysisPostProcessor implements AnalysisPostProcessor<DoopAnalysis> {
 
-    protected Log logger = LogFactory.getLog(getClass())
+	protected Log logger = LogFactory.getLog(getClass())
 
-    @Override
-    void process(DoopAnalysis analysis) {
-        if (!analysis.options.X_SERVER_LOGIC.value && !analysis.options.X_STOP_AT_FACTS.value && !analysis.options.X_STOP_AT_INIT.value && !analysis.options.X_STOP_AT_BASIC.value)
-            printStats(analysis)
-        if (analysis.options.SANITY.value)
-            printSanityResults(analysis)
-        linkResult(analysis)
-    }
+	@Override
+	void process(DoopAnalysis analysis) {
+		if (!analysis.options.X_SERVER_LOGIC.value && !analysis.options.X_STOP_AT_FACTS.value && !analysis.options.X_STOP_AT_INIT.value && !analysis.options.X_STOP_AT_BASIC.value)
+			printStats(analysis)
+		if (analysis.options.SANITY.value)
+			printSanityResults(analysis)
+		linkResult(analysis)
+	}
 
+	void printStats(DoopAnalysis analysis) {
+		def lines = []
 
-    protected void printStats(DoopAnalysis analysis) {
-        def lines = []
+		if (analysis.options.LB3.value) {
+			analysis.processRelation("Stats:Runtime") { String line ->
+				if (!filterOutLBWarn(line)) lines << line
+			}
+		} else {
+			def file = new File("${analysis.database}/Stats_Runtime.csv")
+			file.eachLine { String line -> lines << line.replace("\t", ", ") }
+		}
 
-        if (analysis.options.LB3.value) {
-            analysis.processRelation("Stats:Runtime") { String line ->
-                if (!filterOutLBWarn(line)) lines.add(line)
-            }
-        }
-        else {
-            def file = new File("${analysis.database}/Stats_Runtime.csv")
-            file.eachLine { String line -> lines.add(line.replace("\t", ", ")) }
-        }
+		logger.info "-- Runtime metrics --"
+		lines.sort()*.split(", ").each {
+			printf("%-80s %,d\n", it[0], it[1] as long)
+		}
 
-        logger.info "-- Runtime metrics --"
-        lines.sort()*.split(", ").each {
-            printf("%-80s %,d\n", it[0], it[1] as long)
-        }
+		if (!analysis.options.X_STATS_NONE.value) {
+			lines = []
 
-        if (!analysis.options.X_STATS_NONE.value) {
-            lines = []
+			if (analysis.options.LB3.value) {
+				analysis.processRelation("Stats:Metrics") { String line ->
+					if (!filterOutLBWarn(line)) lines.add(line)
+				}
+			} else {
+				def file = new File("${analysis.database}/Stats_Metrics.csv")
+				file.eachLine { String line -> lines.add(line.replace("\t", ", ")) }
+			}
 
-            if (analysis.options.LB3.value) {
-                analysis.processRelation("Stats:Metrics") { String line ->
-                    if (!filterOutLBWarn(line)) lines.add(line)
-                }
-            }
-            else {
-                def file = new File("${analysis.database}/Stats_Metrics.csv")
-                file.eachLine { String line -> lines.add(line.replace("\t", ", ")) }
-            }
+			logger.info "-- Statistics --"
+			lines.sort()*.split(", ").each {
+				printf("%-80s %,d\n", it[1], it[2] as long)
+			}
+		}
+	}
 
-            logger.info "-- Statistics --"
-            lines.sort()*.split(", ").each {
-                printf("%-80s %,d\n", it[1], it[2] as long)
-            }
-        }
-    }
+	void printSanityResults(DoopAnalysis analysis) {
+		def file = new File("${analysis.database}/Sanity.csv")
+		logger.info "-- Sanity Results --"
+		file.readLines().sort()*.split("\t").each {
+			printf("%-80s %,d\n", it[1], it[2] as long)
+		}
+	}
 
-    protected void printSanityResults(Analysis analysis) {
-        def file = new File("${analysis.database}/Sanity.csv")
-        logger.info "-- Sanity Results --"
-        file.readLines().sort()*.split("\t").each {
-            printf("%-80s %,d\n", it[1], it[2] as long)
-        }
-    }
+	void linkResult(DoopAnalysis analysis) {
+		if (analysis.options.X_STOP_AT_FACTS.value) {
+			logger.info "Making facts available at ${analysis.options.X_STOP_AT_FACTS.value}"
+			return
+		}
 
-    protected void linkResult(Analysis analysis) {
-        if (analysis.options.X_STOP_AT_FACTS.value) {
-            logger.info "Making facts available at ${analysis.options.X_STOP_AT_FACTS.value}"
-            return
-        }
+		def inputName
+		def platform = analysis.options.PLATFORM.value
 
-        def inputName
-        def platform = analysis.options.PLATFORM.value
+		if (analysis.options.X_START_AFTER_FACTS.value)
+			inputName = analysis.id
+		else
+			inputName = FilenameUtils.getBaseName(analysis.inputFiles[0].toString())
 
-        if (analysis.options.X_START_AFTER_FACTS.value)
-            inputName = analysis.id
-        else
-            inputName = FilenameUtils.getBaseName(analysis.inputFiles[0].toString())
+		def humanDatabase = new File("${Doop.doopHome}/results/${inputName}/${analysis.name}/${platform}/${analysis.id}")
+		humanDatabase.mkdirs()
+		if (humanDatabase.exists()) humanDatabase.delete()
+		logger.info "Making database available at $humanDatabase"
+		Files.createSymbolicLink(humanDatabase.toPath(), analysis.database.toPath())
 
-        def humanDatabase = new File("${Doop.doopHome}/results/${inputName}/${analysis.name}/${platform}/${analysis.id}")
-        humanDatabase.mkdirs()
-        logger.info "Making database available at $humanDatabase"
-        analysis.executor.execute(['ln', '-s', '-f', analysis.database, humanDatabase].collect { it as String })
+		def lastAnalysis = new File("${Doop.doopHome}/last-analysis")
+		if (lastAnalysis.exists()) lastAnalysis.delete()
+		logger.info "Making database available at $lastAnalysis"
+		Files.createSymbolicLink(lastAnalysis.toPath(), analysis.database.toPath())
 
-        def lastAnalysis = "${Doop.doopHome}/last-analysis"
-        logger.info "Making database available at $lastAnalysis"
-        analysis.executor.execute(['ln', '-s', '-f', '-n', analysis.database, lastAnalysis].collect { it as String })
+		if (analysis.options.SOUFFLE_PROFILE.value)
+			logger.info "Souffle analysis profile available at ${analysis.outDir}/profile.txt"
+	}
 
-        if (analysis.options.SOUFFLE_PROFILE.value)
-            logger.info "Souffle analysis profile available at ${analysis.outDir}/profile.txt"
-    }
-
-    protected boolean filterOutLBWarn(String line) {
-        return line == '*******************************************************************' ||
-            line == 'Warning: BloxBatch is deprecated and will not be supported in LogicBlox 4.0.' ||
-            line == "Please use 'lb' instead of 'bloxbatch'."
-    }
+	static boolean filterOutLBWarn(String line) {
+		line in ['*******************************************************************',
+		         'Warning: BloxBatch is deprecated and will not be supported in LogicBlox 4.0.',
+		         "Please use 'lb' instead of 'bloxbatch'."]
+	}
 }
