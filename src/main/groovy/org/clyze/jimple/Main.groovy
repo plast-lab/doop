5515bbd19577a3a1842a46b7c08d5473b5e9b440
George Fourtounis <gfour@di.uoa.gr>
2018-08-05 17:11:10 +0300

    Add usage reminder message for jimple2json.

--- src/main/groovy/org/clyze/jimple/Main.groovy
+++ src/main/groovy/org/clyze/jimple/Main.groovy
@@ -2,6 +2,10 @@ package org.clyze.jimple
 
 import static groovy.io.FileType.FILES
 
+if (!args) {
+    System.out.println("Usage: jimple2json jimple-directory");
+}
+
 args.each { dir ->
 	def f = new File(dir)
 	if (f.isDirectory())
