f63fa7ba206a934be1fea9a99078b86d9b168dd3
Anastasios Antoniadis <anantoni@di.uoa.gr>
2018-09-04 20:48:28 +0300

    Changes in oracular.py

--- bin/oracular.py
+++ bin/oracular.py
@@ -2,10 +2,15 @@
 import os
 import shutil
 import sys
-from collections import OrderedDict
-from operator import itemgetter
 
 # This script should be executed from the root directory of Doop.
+
+insens_method_weight_dict = {}
+sens_method_weight_dict = {}
+method_ratio_dict = {}
+insens_method_cost_dict = {}
+sens_method_cost_dict = {}
+sorted_method_ratio_dict = {}
 
 # ----------------- configuration -------------------------
 DOOP = './doop'  # './doopOffline'
@@ -31,12 +36,12 @@ YELLOW = '\033[33m'
 BOLD = '\033[1m'
 
 
-def runPreAnalyses(initArgs):
+def run_pre_analyses(init_args):
     if not os.path.exists('oracular'):
         os.mkdir('oracular')
     if not os.path.exists(ORACULAR_CACHE):
         os.mkdir(ORACULAR_CACHE)
-    args = [DOOP] + initArgs
+    args = [DOOP] + init_args
     args = args + ['-a', PRE_ANALYSIS_1]
     args = args + ["--Xoracular-heuristics"]
     cmd = ' '.join(args)
@@ -48,7 +53,7 @@ def runPreAnalyses(initArgs):
     shutil.copyfile(from_path, dump_path)
 
     print YELLOW + BOLD + 'Running pre-analyses #2 ' + PRE_ANALYSIS_2 + RESET
-    args = [DOOP] + initArgs
+    args = [DOOP] + init_args
     args = args + ['-a', PRE_ANALYSIS_2]
     args = args + ["--Xoracular-heuristics"]
     cmd = ' '.join(args)
@@ -58,91 +63,64 @@ def runPreAnalyses(initArgs):
     shutil.copyfile(from_path, dump_path)
 
 
-def runOracularClassification55():
-    print YELLOW + BOLD + 'Running Oracular classification ' + RESET
-    args = [SOUFFLE] + ['souffle-logic/addons/oracular/method-classification.dl'] + [
-        '--fact-dir={0}'.format(ORACULAR_CACHE)] + ['--output-dir={0}'.format(ORACULAR_CACHE)] + []
-    cmd = ' '.join(args)
-    print cmd
-    os.system(cmd)
-
-
-def runOracularClassification():
+def run_oracular_analysis_classification():
     print YELLOW + BOLD + 'Running Oracular classification ' + RESET
     insens_file = open(ORACULAR_CACHE + "/InsensitiveSum.facts", 'r')
     sens_file = open(ORACULAR_CACHE + "/SensitiveSum.facts", 'r')
-    insens_dict = {}
-    sens_dict = {}
-    method_ratio_dict = {}
 
     for line in insens_file:
         pieces = line.split('\t')
         method = pieces[0]
         weight = int(pieces[1])
-        insens_dict.update({method: weight})
+        insens_method_weight_dict.update({method: weight})
 
-    print YELLOW + BOLD + 'INSENS methods ' + str(len(insens_dict.keys())) + RESET
+    print YELLOW + BOLD + 'INSENS methods ' + str(len(insens_method_weight_dict.keys())) + RESET
+    insens_file.close()
 
     for line in sens_file:
         pieces = line.split('\t')
         method = pieces[0]
         weight = int(pieces[1])
-        sens_dict.update({method: weight})
+        sens_method_weight_dict.update({method: weight})
 
-    print YELLOW + BOLD + 'SENS methods ' + str(len(sens_dict.keys())) + RESET
-
-
-    insens_file.close()
+    print YELLOW + BOLD + 'SENS methods ' + str(len(sens_method_weight_dict.keys())) + RESET
     sens_file.close()
-    missing_methods = 0
 
+    missing_methods = 0
     special_cs_file = open(ORACULAR_CACHE + "/SpecialCSMethods.csv", "w")
 
-    for method in insens_dict.keys():
-        if insens_dict.get(method) == 0:
+    for method in insens_method_weight_dict.keys():
+        if insens_method_weight_dict.get(method) == 0:
             method_ratio_dict.update({method: 0.0})
         else:
-            if sens_dict.__contains__(method):
-                insensWeight = insens_dict.get(method)
-                sensWeight = sens_dict.get(method)
-                ratio = float(sensWeight) / float(insensWeight)
+            if sens_method_weight_dict.__contains__(method):
+                insens_weight = insens_method_weight_dict.get(method)
+                sens_weight = sens_method_weight_dict.get(method)
+                ratio = float(sens_weight) / float(insens_weight)
                 method_ratio_dict.update({method: ratio})
             else:
                 special_cs_file.write(method + "\t" "2-object\n")
                 missing_methods += 1
 
-    print YELLOW + BOLD + "Missing methods: " + str(missing_methods)
+    sorted_method_ratio_dict = sorted(method_ratio_dict.items(), key=lambda x: x[1])
 
+    optimal_ratio_threshold = binary_search_threshold(method_ratio_dict.values())
 
-    #method_ratio_list = sorted(method_ratio_dict.items(), key=lambda (k, v): v)
-
-    print YELLOW + BOLD + 'Ratio methods ' + str(len(method_ratio_dict.keys())) + RESET
-
-
-    method_ratio_sum = 0.0
-    method_num = 0
     two_object_sensitive_methods = 0
     two_type_sensitive_methods = 0
     one_type_sensitive_methods = 0
     context_insensitive_methods = 0
-    #print YELLOW + BOLD + "Method ratio list size: " + str(len(method_ratio_list))
-    for method, ratio in sorted(method_ratio_dict.iteritems(), key=lambda (k, v): (v, k)):
-        #print method + "\t" + str(ratio)
-        method_ratio_sum += ratio
-        method_num += 1
-        method_ratio_average = float(method_ratio_sum) / float(method_num)
+    for method, ratio in sorted_method_ratio_dict:
 
-        #print YELLOW + BOLD + "Method num: " + str(method_num) + "Ratio average: " + str(method_ratio_average) + RESET
-
-        if method_ratio_average <= TWO_OBJECT_THRESHOLD:
+        if ratio <= optimal_ratio_threshold:
             special_cs_file.write(method + "\t" "2-object\n")
             two_object_sensitive_methods += 1
-        elif method_ratio_average <= TWO_TYPE_THRESHOLD:
-            special_cs_file.write(method + "\t" + "2-type\n")
-            two_type_sensitive_methods += 1
-        elif method_ratio_average <= ONE_TYPE_THRESHOLD:
-            special_cs_file.write(method + "\t" + "1-type\n")
-            one_type_sensitive_methods += 1
+        # elif method_ratio_average <= TWO_TYPE_THRESHOLD:
+        #     special_cs_file.write(method + "\t" + "2-type\n")
+        #     two_type_sensitive_methods += 1
+        # elif method_ratio_average <= ONE_TYPE_THRESHOLD:
+        #     special_cs_file.write(method + "\t" + "1-type\n")
+        #     one_type_sensitive_methods += 1
         else:
             special_cs_file.write(method + "\t" + "context-insensitive\n")
             context_insensitive_methods += 1
@@ -155,7 +133,42 @@ def runOracularClassification():
     print YELLOW + BOLD + "context-insensitive methods: " + str(context_insensitive_methods) + RESET
 
 
-def runMainAnalysis(args, oracular_file):
+def calculate_analysis_threshold(ratio_threshold):
+    analysis_weight = 0
+    for method, ratio in sorted_method_ratio_dict:
+        if ratio <= ratio_threshold:
+            analysis_weight += sens_method_cost_dict.get(method)
+        else:
+            analysis_weight += insens_method_cost_dict.get(method)
+    return analysis_weight
+
+
+def binary_search_threshold(threshold_list):
+    if 2 < calculate_analysis_threshold(threshold_list[0]):
+        return threshold_list[0]
+    if 2 > calculate_analysis_threshold(threshold_list[len(threshold_list) - 1]):
+        return threshold_list[len(threshold_list)-1]
+
+    low = 0
+    high = len(threshold_list) - 1
+
+    while low >= high:
+        mid = low + (high - low) / 2
+
+        if 2 < calculate_analysis_threshold(threshold_list[mid]):
+            high = mid - 1
+        elif 2 > calculate_analysis_threshold(threshold_list[mid]):
+            low = mid + 1
+        else:
+            return threshold_list[mid]
+
+    if (calculate_analysis_threshold(threshold_list[low]) - 2) < (2 - calculate_analysis_threshold(threshold_list[high])):
+        return threshold_list[low]
+    else:
+        return threshold_list[high]
+
+
+def run_main_analysis(args, oracular_file):
     args = [DOOP] + args
     args = args + ['-a', MAIN_ANALYSIS]
     args = args + ['--special-cs-methods', oracular_file]
@@ -166,9 +179,9 @@ def runMainAnalysis(args, oracular_file):
 
 
 def run(args):
-    #runPreAnalyses(args)
-    runOracularClassification()
-    runMainAnalysis(args, ORACULAR_CACHE + "/SpecialCSMethods.csv")
+    # runPreAnalyses(args)
+    run_oracular_analysis_classification()
+    run_main_analysis(args, ORACULAR_CACHE + "/SpecialCSMethods.csv")
 
 
 if __name__ == '__main__':
