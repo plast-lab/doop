19579d19993f481ff98ab84b8853d500075a1aa7
George Fourtounis <gfour@di.uoa.gr>
2018-09-24 23:53:17 +0300

    Add platforms test.

---
+++ src/test/groovy/org/clyze/doop/PlatformTest.groovy
@@ -0,0 +1,37 @@
+package org.clyze.doop
+
+import org.clyze.analysis.Analysis
+import org.clyze.doop.input.PlatformManager
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * Test that all platforms can be selected without crashing Doop.
+ */
+class PlatformTest extends Specification {
+
+    Analysis analysis
+
+    @Unroll
+    def "Platform test [#platform]"(String platform) {
+        when:
+        Main.main((String[])["-i", "http://centauri.di.uoa.gr:8081/artifactory/Demo-benchmarks/test-resources/006-hello-world-1.2.jar",
+                             "-a", "context-insensitive",
+                             "--id", "dry-run-${platform}",
+                             "--platform", platform,
+                             "--Xdry-run"])
+        analysis = Main.analysis
+
+        then:
+        someFileExists()
+
+        where:
+        platform << PlatformManager.ARTIFACTS_FOR_PLATFORM.keySet().findAll { !(it.contains("python")) }
+    }
+
+    // Trivial check.
+    void someFileExists() {
+        assert true == (new File("${analysis.outDir}/meta")).exists()
+    }
+
+}
