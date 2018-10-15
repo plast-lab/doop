2bb5ae2c5f4523f10d828c170ef831b3386eb44e
George Fourtounis <gfour@di.uoa.gr>
2018-10-15 15:44:32 +0300

    Separate native code test.

--- src/test/groovy/org/clyze/doop/SimpleAnalysisTests.groovy
+++ src/test/groovy/org/clyze/doop/SimpleAnalysisTests.groovy
@@ -11,18 +11,7 @@ import static org.clyze.doop.TestUtils.*
  */
 class SimpleAnalysisTests extends ServerAnalysisTests {
 
-    // @spock.lang.Ignore
-	@Unroll
-	def "Server analysis test 009 (native code)"() {
-		when:
-		analyzeTest("009-native", ["--simulate-native-returns"])
-
-		then:
-		varPointsTo(analysis, '<HelloJNI: void main(java.lang.String[])>/obj#_34', '<native java.lang.Object value allocated in <HelloJNI: java.lang.Object newJNIObj()>>')
-		varPointsTo(analysis, '<HelloJNI: void main(java.lang.String[])>/list#_43', '<java.io.UnixFileSystem: java.lang.String[] list(java.io.File)>/new java.lang.String[]/0')
-	}
-
-	// @spock.lang.Ignore
+	@spock.lang.Ignore
 	@Unroll
 	def "Server analysis test 012 (interface fields)"() {
 		when:
