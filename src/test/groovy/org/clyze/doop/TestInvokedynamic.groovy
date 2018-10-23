11dbc3665d0731b9c00ed59572d68649e1ff2939
George Fourtounis <gfour@di.uoa.gr>
2018-10-23 13:57:47 +0300

    Fix identifier in test.

--- src/test/groovy/org/clyze/doop/TestInvokedynamic.groovy
+++ src/test/groovy/org/clyze/doop/TestInvokedynamic.groovy
@@ -18,7 +18,7 @@ class TestInvokedynamic extends ServerAnalysisTests {
 		analyzeTest("107-lambdas", ["--platform", "java_8", "--Xserver-logic", "--Xextra-logic", "${Doop.souffleAddonsPath}/testing/test-exports.dl", "--generate-jimple"])
 
 		then:
-		varPointsToQ(analysis, '<Main: void main(java.lang.String[])>/intWriter#_12', '<Main: void main(java.lang.String[])>/invokedynamic_Main::lambda$main$0/0::: java.util.function.Function::: (Mock)::: reference Main::lambda$main$0 from <Main: java.lang.String lambda$main$0(java.lang.Integer)> wrapped as java.util.function.Function.apply')
+		varPointsToQ(analysis, '<Main: void main(java.lang.String[])>/intWriter#_13', '<Main: void main(java.lang.String[])>/invokedynamic_Main::lambda$main$0/0::: java.util.function.Function::: (Mock)::: reference Main::lambda$main$0 from <Main: java.lang.String lambda$main$0(java.lang.Integer)> wrapped as java.util.function.Function.apply')
 		invokedynamicCGE(analysis, '<Main: void main(java.lang.String[])>/java.util.function.Function.apply/0', '<Main: java.lang.String lambda$main$0(java.lang.Integer)>')
 		invoValue(analysis, '<A: java.lang.Integer lambda$new$0(java.lang.Integer,java.lang.Integer)>/java.lang.Integer.compareTo/0', '<java.lang.Integer: int compareTo(java.lang.Integer)>')
 		methodIsReachable(analysis, '<java.lang.invoke.InnerClassLambdaMetafactory: java.lang.invoke.CallSite buildCallSite()>')
