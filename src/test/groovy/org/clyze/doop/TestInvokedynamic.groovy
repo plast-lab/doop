663c04860de917b890a042c8ee4cf776fd202730
George Fourtounis <gfour@di.uoa.gr>
2018-10-26 22:10:01 +0300

    Improve invokedynamic test.

--- src/test/groovy/org/clyze/doop/TestInvokedynamic.groovy
+++ src/test/groovy/org/clyze/doop/TestInvokedynamic.groovy
@@ -34,6 +34,8 @@ class TestInvokedynamic extends ServerAnalysisTests {
 		then:
 		varPointsTo(analysis, '<Main: void main(java.lang.String[])>/c1#_15', '<Main: void main(java.lang.String[])>/invokedynamic_A::meth1234/0::: java.util.function.Consumer::: (Mock)::: reference A::meth1234 from <A: void meth1234(java.lang.Integer)> wrapped as java.util.function.Consumer.accept')
 		varPointsTo(analysis, '<Main: void main(java.lang.String[])>/c3#_52', '<Main: void main(java.lang.String[])>/invokedynamic_A::meth99/0::: java.util.function.Function::: (Mock)::: reference A::meth99 from <A: java.lang.Integer meth99(java.lang.Integer)> wrapped as java.util.function.Function.apply')
+		varPointsTo(analysis, '<A: void meth1234(java.lang.Integer)>/x#_0', '<Main: void main(java.lang.String[])>/new java.lang.Integer/0')
+		varPointsTo(analysis, '<A: java.lang.Integer meth99(java.lang.Integer)>/this#_0', '<Main: void main(java.lang.String[])>/new A/0')
 		noSanityErrors(analysis)
 	}
 }
