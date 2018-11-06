47bb9b80c3accfa405d4b37d98edaa1d211cb8ec
Tony Antoniadis <anastasios.antoniadis@gmail.com>
2018-11-06 17:02:37 +0200

    Changes in export.dl and simple statistics, update partitioned analysis

--- src/main/groovy/org/clyze/doop/core/SoufflePartitionedAnalysis.groovy
+++ src/main/groovy/org/clyze/doop/core/SoufflePartitionedAnalysis.groovy
@@ -133,36 +133,93 @@ class SoufflePartitionedAnalysis extends SouffleAnalysis {
             analysisExecutorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS)
 
             partitionNumber = 0
-            def insPrimaryVPT = 0
-            def sensPrimaryVPT = 0
 
-            def insNonPrimaryVPTSet = [] as Set
-	        def reachableMethodsSet = [] as Set
+            def insVPTSet = [] as HashSet
+            def insReachableSet = [] as HashSet
+	        def insCallGraphEdgeSet = [] as HashSet
+	        def mayFailCastSet = [] as HashSet
+	        def polymorphicCallSiteSet = [] as HashSet
+	        def insVPTAppSet = [] as HashSet
+	        def insReachableAppSet = [] as HashSet
+	        def mayFailCastAppSet = [] as HashSet
+	        def polymorphicCallSiteAppSet = [] as HashSet
+
             partitions.each { partition ->
                 partitionNumber++
                 def childDatabaseDir = new File(outDir.canonicalPath + "-part-" + partitionNumber + File.separator + "database")
 
-                insPrimaryVPT += Files.lines(new File(childDatabaseDir, "Stats_Simple_InsensPrimaryVarPointsTo.csv").toPath()).count()
-                sensPrimaryVPT += Files.lines(new File(childDatabaseDir, "Stats_Simple_PrimaryVarPointsTo.csv").toPath()).count()
-
-                def insNoPrimaryVPTFile = new File(childDatabaseDir, "Stats_Simple_InsensNonPrimaryVarPointsTo.csv")
-                lines = insNoPrimaryVPTFile.readLines()
-
-                lines.each() {line ->
-                    insNonPrimaryVPTSet.add(line)
+                def file = new File(childDatabaseDir, "VarPointsTo.csv")
+	            def line
+                BufferedReader reader = file.newReader()
+	            while ((line = reader.readLine()) != null) {
+                    insVPTSet.add(line)
                 }
 
-	            def reachableFile = new File(childDatabaseDir, "Reachable.csv")
-	            lines = reachableFile.readLines()
+	            file = new File(childDatabaseDir, "Reachable.csv")
+	            reader = file.newReader()
 
-	            lines.each() {line ->
-		            reachableMethodsSet.add(line)
+	            while ((line = reader.readLine()) != null) {
+		            insReachableSet.add(line)
+	            }
+
+	            file = new File(childDatabaseDir, "CallGraphEdge.csv")
+	            reader = file.newReader()
+
+	            while ((line = reader.readLine()) != null) {
+		            insCallGraphEdgeSet.add(line)
+	            }
+
+	            file = new File(childDatabaseDir, "MayFailCast.csv")
+	            reader = file.newReader()
+
+	            while ((line = reader.readLine()) != null) {
+		            mayFailCastSet.add(line)
+	            }
+
+	            file = new File(childDatabaseDir, "PolymorphicCallSite.csv")
+	            reader = file.newReader()
+
+	            while ((line = reader.readLine()) != null) {
+		            polymorphicCallSiteSet.add(line)
+	            }
+
+	            file = new File(childDatabaseDir, "VarPointsToApp.csv")
+	            reader = file.newReader()
+	            while ((line = reader.readLine()) != null) {
+		            insVPTAppSet.add(line)
+	            }
+
+	            file = new File(childDatabaseDir, "ReachableApp.csv")
+	            reader = file.newReader()
+
+	            while ((line = reader.readLine()) != null) {
+		            insReachableAppSet.add(line)
+	            }
+
+	            file = new File(childDatabaseDir, "MayFailCastApp.csv")
+	            reader = file.newReader()
+
+	            while ((line = reader.readLine()) != null) {
+		            mayFailCastAppSet.add(line)
+	            }
+
+	            file = new File(childDatabaseDir, "PolymorphicCallSiteApp.csv")
+	            reader = file.newReader()
+
+	            while ((line = reader.readLine()) != null) {
+		            polymorphicCallSiteAppSet.add(line)
 	            }
             }
 
-            statsMetricsFile.append("1.0\tvar points-to (INS)\t${insPrimaryVPT + insNonPrimaryVPTSet.size()}\n")
-            statsMetricsFile.append("1.5\tvar points-to (SENS)\t${sensPrimaryVPT + insNonPrimaryVPTSet.size()}\n")
-	        statsMetricsFile.append("11.5\treachable methods (INS)\t${reachableMethodsSet.size()}\n")
+            statsMetricsFile.append("1.0\tvar points-to (INS)\t${insVPTSet.size()}\n")
+	        statsMetricsFile.append("8.0\tcall graph edges (INS)\t${insCallGraphEdgeSet.size()}\n")
+	        statsMetricsFile.append("11.0\treachable methods (INS)\t${insReachableSet.size()}\n")
+	        statsMetricsFile.append("22.0\treachable casts that may fail\t${mayFailCastSet.size()}\n")
+	        statsMetricsFile.append("14.0\tpolymorphic call sites\t${polymorphicCallSiteSet.size()}\n")
+	        statsMetricsFile.append("5.0\tapp var points-to (INS)\t${insVPTAppSet.size()}\n")
+	        statsMetricsFile.append("25.5\tapp reachable methods (INS)\t${insReachableAppSet.size()}\n")
+	        statsMetricsFile.append("25.0\tapp reachable casts that may fail\t${mayFailCastAppSet.size()}\n")
+	        statsMetricsFile.append("18.0\tapp polymorphic call sites\t${polymorphicCallSiteAppSet.size()}\n")
 
             int dbSize = (sizeOfDirectory(database) / 1024).intValue()
 
