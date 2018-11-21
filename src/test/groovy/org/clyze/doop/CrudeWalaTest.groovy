e5b5d610692206da9448bc4195f2ccfb2b0ef1f6
George Fourtounis <gfour@di.uoa.gr>
2018-11-21 17:10:48 +0200

    Turn on sanity checking in the WALA test.

--- src/test/groovy/org/clyze/doop/CrudeWalaTest.groovy
+++ src/test/groovy/org/clyze/doop/CrudeWalaTest.groovy
@@ -15,12 +15,13 @@ class CrudeWalaTest extends Specification {
 	@Unroll
 	def "Crude testing WALA fact generation and analysis"() {
 		when:
-		Main.main((String[])["-i", "http://centauri.di.uoa.gr:8081/artifactory/DaCapo-benchmarks/2006/antlr.jar", "-a", "context-insensitive", "--id", "antlr-wala", "--dacapo", "--wala-fact-gen", "--platform", "java_8"])
+		Main.main((String[])["-i", "http://centauri.di.uoa.gr:8081/artifactory/DaCapo-benchmarks/2006/antlr.jar", "-a", "context-insensitive", "--id", "antlr-wala", "--dacapo", "--wala-fact-gen", "--platform", "java_8", "--sanity"])
 		analysis = Main.analysis
 
 		then:
 		relationHasApproxSize(analysis, "CallGraphEdge", 63974)
 		relationHasApproxSize(analysis, "VarPointsTo", 2755187)
 		relationHasApproxSize(analysis, "ReachableContext", 9297)
+		noSanityErrors(analysis)
 	}
 }
