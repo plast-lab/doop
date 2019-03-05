34f95d265e87f573dd964ac120927bc72e804aa4
George Fourtounis <gfour@di.uoa.gr>
2019-03-06 00:01:41 +0200

    Port TestReflection to logic.

--- src/test/groovy/org/clyze/doop/TestReflection.groovy
+++ src/test/groovy/org/clyze/doop/TestReflection.groovy
@@ -14,22 +14,22 @@ class TestReflection extends ServerAnalysisTests {
 		Analysis analysis = analyzeTest("016-reflection",
 										["--reflection-classic", "--reflection-dynamic-proxies",
 										 "--gen-opt-directives", "--Xserver-logic", "--generate-jimple",
-										 "--Xextra-logic", "${Doop.souffleAddonsPath}/testing/test-exports.dl",
+										 "--Xextra-logic", "${Doop.souffleAddonsPath}/testing/TestReflection.dl",
 										 "--thorough-fact-gen", "--sanity",
 										 "--platform", "java_8"])
 
 		then:
-		varPointsToQ(analysis, '<Main: void main(java.lang.String[])>/cA#_31', '<class A>')
-		varPointsToQ(analysis, '<Main: void main(java.lang.String[])>/cA_2#_39', '<class A>')
-		varPointsToQ(analysis, '<Main: void main(java.lang.String[])>/constr#_32', '<<reified constructor <A: void <init>()>>>')
-		varPointsToQ(analysis, '<Main: void main(java.lang.String[])>/constr2#_42', '<<reified constructor <A: void <init>(java.lang.Integer,B)>>>')
-		varPointsToQ(analysis, '<Main: void main(java.lang.String[])>/a3#_33', '<reflective Class.newInstance/new A>')
-		varPointsToQ(analysis, '<Main: void main(java.lang.String[])>/a3_2#_35', '<reflective Constructor.newInstance/new A>')
-		varPointsToQ(analysis, '<Main: void main(java.lang.String[])>/iField#_46', '<<reified field <A: java.lang.Integer i>>>')
-		varPointsToQ(analysis, '<Main: void main(java.lang.String[])>/bFieldVal#_52', '<A: void <init>()>/new B/0')
-		varPointsToQ(analysis, '<Main: void main(java.lang.String[])>/bFieldValB#_53', '<A: void <init>()>/new B/0')
-		varPointsToQ(analysis, '<Main: void testProxies()>/g#_109', '<proxy object for interface G at <Main: void testProxies()>/java.lang.reflect.Proxy.newProxyInstance/0>')
-		proxyCGE(analysis, '<Main: void testProxies()>/G.countInteger/0', '<DHandler: java.lang.Object invoke(java.lang.Object,java.lang.reflect.Method,java.lang.Object[])>')
+		testSucceeds(analysis, "test1.1")
+		testSucceeds(analysis, "test1.2")
+		testSucceeds(analysis, "test1.3")
+		testSucceeds(analysis, "test1.4")
+		testSucceeds(analysis, "test1.5")
+		testSucceeds(analysis, "test1.6")
+		testSucceeds(analysis, "test1.7")
+		testSucceeds(analysis, "test1.8")
+		testSucceeds(analysis, "test1.9")
+		testSucceeds(analysis, "test1.10")
+		testSucceeds(analysis, "test1.11")
 		noSanityErrors(analysis)
 	}
 
