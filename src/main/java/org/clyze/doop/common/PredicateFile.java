a0f16b2ed01c06ce460db6ca1440caa7848ba0bd
George Fourtounis <gfour@di.uoa.gr>
2019-03-26 15:08:25 +0200

    Generate facts for annotation elements.

--- src/main/java/org/clyze/doop/common/PredicateFile.java
+++ src/main/java/org/clyze/doop/common/PredicateFile.java
@@ -13,6 +13,7 @@ public enum PredicateFile
     ACTUAL_PARAMETER("ActualParam"),
     ANDROID_APPLICATION("AndroidApplication"),
     ANDROID_ENTRY_POINT("AndroidEntryPoint"),
+    ANNOTATION_ELEMENT("AnnotationElement"),
     APP_CLASS("ApplicationClass"),
     APP_PACKAGE("ApplicationPackage"),
     ARRAY_INITIAL_VALUE_FROM_LOCAL("ArrayInitialValueFromLocal"),
