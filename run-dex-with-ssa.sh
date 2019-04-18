0de8a749f168ace2ac68e92395721f089ce84ea4
George Fourtounis <gfour@di.uoa.gr>
2019-04-18 15:01:02 +0300

    Update dex script.

--- run-dex-with-ssa.sh
+++ run-dex-with-ssa.sh
@@ -3,24 +3,26 @@
 # Test script for running the dex front end with intermediate SSA
 # transformation.
 
-if [ "$1" == "" ]; then
-    echo "Usage: run-dex-with-ssa.sh app"
+if [ "$1" == "" ] || [ "$2" == "" ]; then
+    echo "Usage: run-dex-with-ssa.sh <apk> <final-facts-out>"
     exit
 else
     APP="$1"
     APP_NAME=$(basename "$1" | sed -e 's/[()]/_/g')
 fi
 
-FACTS_IN=${ANDROID_IN}
-FACTS_OUT=${ANDROID_OUT}
+ID=${APP_NAME}-dex-facts
+FACTS_IN=${ID}
+FACTS_OUT=$(realpath $2)
 SSA_TRANSFORMER=$(realpath souffle-scripts/ssa-transform.dl)
 
-./doop -i ${APP} -a context-insensitive --id ${APP_NAME}-dex-facts --platform android_25_fulljars --dex --Xstop-at-facts ${FACTS_IN}  --cache 
+echo "APP_NAME=${APP_NAME}"
 
-./gradlew souffleScript -Pargs="${SSA_TRANSFORMER} ${FACTS_IN} ${FACTS_OUT} ${DOOP_HOME}/cache 26 true false false false"
+./doop -i ${APP} -a context-insensitive --id ${ID} --platform android_25_fulljars --dex --Xfacts-subset APP --Xstop-at-facts ${FACTS_IN} --cache
+
+./gradlew souffleScript -Pargs="${SSA_TRANSFORMER} ${FACTS_IN} ${FACTS_OUT} ${DOOP_HOME}/cache 26 false false false false"
 
 pushd "${FACTS_OUT}/database"
-echo "TODO: move"
 
 for file in ${FACTS_IN}/*.facts; do
     filename=$(basename "${file}")
