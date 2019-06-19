be8ee32fe27513f39db39be0e6b12049f7b03b83
George Fourtounis <gfour@di.uoa.gr>
2019-06-19 16:50:07 +0000

    Fix spaces.

--- src/test/groovy/org/clyze/doop/DoopSpec.groovy
+++ src/test/groovy/org/clyze/doop/DoopSpec.groovy
@@ -9,19 +9,19 @@ import static org.clyze.doop.TestUtils.*
 // A class that initializes Doop before running its tests. Tests
 // should reuse this class, if they read Doop paths.
 abstract class DoopSpec extends Specification {
-	def setupSpec() {
-		Doop.initDoopFromEnv()
+    def setupSpec() {
+	Doop.initDoopFromEnv()
     }
 
-	Analysis analyzeTest(String test, String jar, List<String> extraArgs, String analysisName = "context-insensitive", String id = null) {
-		String analysisId = id ?: "test-${test}"
-		List args = ["-i", jar,
-					 "-a", analysisName,
-					 "--id", analysisId,
-					 "--Xstats-full"] + extraArgs
-		Main.main2((String[])args)
-		return Main.analysis
-	}
+    Analysis analyzeTest(String test, String jar, List<String> extraArgs, String analysisName = "context-insensitive", String id = null) {
+	String analysisId = id ?: "test-${test}"
+	    List args = ["-i", jar,
+			 "-a", analysisName,
+			 "--id", analysisId,
+			 "--Xstats-full"] + extraArgs
+	    Main.main2((String[])args)
+	    return Main.analysis
+    }
 
     Analysis analyzeBuiltinTest(String test, List<String> extraArgs, String analysisName = "context-insensitive", String id = null) {
         return analyzeTest(test, testJar(test), extraArgs, analysisName, id)
