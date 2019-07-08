d48c4e849cb24b477dac6807c7bfb5fd2642a731
George Fourtounis <gfour@di.uoa.gr>
2019-07-08 20:22:35 +0300

    Estimate initial StringBuilder capacity when adding fact lines.

--- src/main/java/org/clyze/doop/common/Database.java
+++ src/main/java/org/clyze/doop/common/Database.java
@@ -63,7 +63,13 @@ public class Database implements Closeable, Flushable {
 
     public void add(PredicateFile predicateFile, String arg, String... args) {
         try {
-            StringBuilder line = new StringBuilder(addColumn(arg));
+            // Estimate StringBuilder capacity.
+            int capacity = args.length + 1;
+            for (String arg0 : args)
+                capacity += arg0.length();
+
+            StringBuilder line = new StringBuilder(capacity);
+            line.append(addColumn(arg));
             for (String col : args) {
                 line.append(SEP);
                 line.append(addColumn(col));
