591552b48a133532965253436a529179cddcc82a
Anastasios Antoniadis <anantoni@di.uoa.gr>
2019-11-11 10:04:18 +0200

    Bug fix in thread.dl rules

---
+++ src/main/groovy/org/clyze/doop/utils/scaler/WorstCaseContextMap.groovy
@@ -0,0 +1,10 @@
+package org.clyze.doop.utils.scaler;
+
+import java.util.Map;
+
+public class AnalysisWorstCaseContext {
+
+	private String analysisName;
+	private Map<String, Long> worstCaseContextMap
+
+}
