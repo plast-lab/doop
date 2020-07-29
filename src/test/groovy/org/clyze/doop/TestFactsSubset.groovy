b3d920269a9dac799e0208dea100cc739e7f6e62
George Fourtounis <gfour@di.uoa.gr>
2020-07-29 16:53:58 +0300

    Fix facts-subset test to cover all cases.

--- src/test/groovy/org/clyze/doop/TestFactsSubset.groovy
+++ src/test/groovy/org/clyze/doop/TestFactsSubset.groovy
@@ -4,13 +4,13 @@ import org.clyze.analysis.Analysis
 import org.clyze.doop.common.Parameters
 import spock.lang.Unroll
 
-class FactsSubsetTest extends ServerAnalysisTests {
+class TestFactsSubset extends ServerAnalysisTests {
 
     // @spock.lang.Ignore
     @Unroll
-    def "Server analysis test 006 (hello world) / facts-subset-APP"() {
+    def "Server analysis test 006 (hello world) / facts-subset"(String subset) {
         when:
-        Analysis analysis = analyzeTest("006-hello-world", ["--Xfacts-subset", "APP", "--platform", "java_8"])
+        Analysis analysis = analyzeTest("006-hello-world", ["--Xfacts-subset", subset, "--platform", "java_8"])
 
         then:
         true
