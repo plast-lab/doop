80473691e4f8af91acd4e788b58a864ef9fbbc72
George Fourtounis <gfour@di.uoa.gr>
2020-12-11 18:41:33 +0200

    Improve exception error display.

--- src/main/groovy/org/clyze/doop/Main.groovy
+++ src/main/groovy/org/clyze/doop/Main.groovy
@@ -32,8 +32,9 @@ class Main {
 		try {
 			main2(args)
 		} catch (e) {
-			if (!(e instanceof DoopErrorCodeException))
-				Help.usage(null, CommandLineAnalysisFactory.createCliBuilder())
+			log.error "ERROR: ${describeError(e)}"
+//			if (!(e instanceof DoopErrorCodeException))
+//				Help.usage(null, CommandLineAnalysisFactory.createCliBuilder())
 		}
 	}
 
@@ -179,5 +180,9 @@ class Main {
 			log.info "Invalid user supplied timeout: `${userTimeout}` - fallback to default (${defaultTimeout})."
 			return defaultTimeout
 		}
+	}
+
+	private static String describeError(Throwable t) {
+		return (t.message != null) ? t.message : (t.cause != null) ? describeError(t.cause) : "unknown"
 	}
 }
