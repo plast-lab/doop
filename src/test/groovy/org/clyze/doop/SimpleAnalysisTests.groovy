66c26ec81b833b4c7c8fbc327583499e709129e5
George Fourtounis <gfour@di.uoa.gr>
2021-03-31 20:02:11 +0300

    Fix sanity check for test 011-variance and preanalyses/Python/light-refelction logic.

--- src/test/groovy/org/clyze/doop/SimpleAnalysisTests.groovy
+++ src/test/groovy/org/clyze/doop/SimpleAnalysisTests.groovy
@@ -10,49 +10,50 @@ import static org.clyze.doop.TestUtils.*
  */
 class SimpleAnalysisTests extends ServerAnalysisTests {
 
-	// @spock.lang.Ignore
-	@Unroll
-	def "Server analysis test 011-variance (additional command-line options)"() {
-		when:
-		// Test additional command-line options.
-		Analysis analysis = analyzeTest("011-variance",
-										["--coarse-grained-allocation-sites",
-										 "--generate-jimple", "--server-logic",
-										 "--cs-library", "--extra-metrics", "--sarif",
-										 "--thorough-fact-gen", "--cfg",
-										 "--platform", "java_8"] + sanityOpts)
+    // @spock.lang.Ignore
+    @Unroll
+    def "Server analysis test 011-variance (additional command-line options)"() {
+        when:
+        // Test additional command-line options.
+        Analysis analysis = analyzeTest("011-variance",
+                                        ["--coarse-grained-allocation-sites",
+                                         "--server-logic", "--thorough-fact-gen",
+					 "--generate-jimple",
+					 "--souffle-run-interpreted",
+                                         "--cs-library", "--extra-metrics", "--sarif",
+                                         "--cfg", "--platform", "java_8"] + sanityOpts)
 
-		then:
-		// Test variance.
-		invoValue(analysis, '<Main: void main(java.lang.String[])>/I.meth1/0', '<Test: java.lang.Object meth1(Clazz)>')
-		invoValue(analysis, '<Main: void main(java.lang.String[])>/I.meth1/1', '<Test: java.lang.Object meth1(Clazz)>')
-		invoValue(analysis, '<Main: void main(java.lang.String[])>/I.meth2/0', '<Test: Clazz meth2(java.lang.Object)>')
-		invoValue(analysis, '<Main: void main(java.lang.String[])>/I.meth2/0', '<Test: SubClazz meth2(java.lang.Object)>')
-		methodIsReachable(analysis, '<Test: SubClazz meth2(java.lang.Object)>')
-		methodSub(analysis, '<I: Clazz meth2(java.lang.Object)>', '<Test: SubClazz meth2(java.lang.Object)>')
-		noSanityErrors(analysis)
-	}
+        then:
+        // Test variance.
+        invoValue(analysis, '<Main: void main(java.lang.String[])>/I.meth1/0', '<Test: java.lang.Object meth1(Clazz)>')
+        invoValue(analysis, '<Main: void main(java.lang.String[])>/I.meth1/1', '<Test: java.lang.Object meth1(Clazz)>')
+        invoValue(analysis, '<Main: void main(java.lang.String[])>/I.meth2/0', '<Test: Clazz meth2(java.lang.Object)>')
+        invoValue(analysis, '<Main: void main(java.lang.String[])>/I.meth2/0', '<Test: SubClazz meth2(java.lang.Object)>')
+        methodIsReachable(analysis, '<Test: SubClazz meth2(java.lang.Object)>')
+        methodSub(analysis, '<I: Clazz meth2(java.lang.Object)>', '<Test: SubClazz meth2(java.lang.Object)>')
+        noSanityErrors(analysis)
+    }
 
-	// @spock.lang.Ignore
-	@Unroll
-	def "Server analysis test 012 (interface fields)"() {
-		when:
-		Analysis analysis = analyzeTest("012-interface-fields", testExports)
+    // @spock.lang.Ignore
+    @Unroll
+    def "Server analysis test 012 (interface fields)"() {
+        when:
+        Analysis analysis = analyzeTest("012-interface-fields", testExports)
 
-		then:
-		staticFieldPointsTo(analysis, '<Y: Y fooooooooo>', '<Y: void <clinit>()>/new Y$1/0')
-	}
+        then:
+        staticFieldPointsTo(analysis, '<Y: Y fooooooooo>', '<Y: void <clinit>()>/new Y$1/0')
+    }
 
-	// @spock.lang.Ignore
-	@Unroll
-	def "Server analysis test 013-enums (reflection, jphantom, default stats)"() {
-		when:
-		Analysis analysis = analyzeTest("013-enums", ["--reflection-classic", "--generate-jimple",
-													  "--stats", "default", "--no-standard-exports",
-													  "--run-jphantom"] + testExports)
+    // @spock.lang.Ignore
+    @Unroll
+    def "Server analysis test 013-enums (reflection, jphantom, default stats)"() {
+        when:
+        Analysis analysis = analyzeTest("013-enums", ["--reflection-classic", "--generate-jimple",
+                                                      "--stats", "default", "--no-standard-exports",
+                                                      "--run-jphantom"] + testExports)
 
-		then:
-		varPointsToQ(analysis, '<Main: void main(java.lang.String[])>/enumConsts#_93', '<Enums array for Main$UndeletablePrefKey>')
-		arrayIndexPointsTo(analysis, '<Enums array for Main$UndeletablePrefKey>', '<Main$UndeletablePrefKey: void <clinit>()>/new Main$UndeletablePrefKey/0', true)
-	}
+        then:
+        varPointsToQ(analysis, '<Main: void main(java.lang.String[])>/enumConsts#_93', '<Enums array for Main$UndeletablePrefKey>')
+        arrayIndexPointsTo(analysis, '<Enums array for Main$UndeletablePrefKey>', '<Main$UndeletablePrefKey: void <clinit>()>/new Main$UndeletablePrefKey/0', true)
+    }
 }
