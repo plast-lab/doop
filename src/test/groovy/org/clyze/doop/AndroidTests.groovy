09bba4bc8862e9eafa23c7ea9b54e2fccee4622a
George Fourtounis <gfour@di.uoa.gr>
2021-04-13 12:31:19 +0300

    Update Android test with new mock object labels.

--- src/test/groovy/org/clyze/doop/AndroidTests.groovy
+++ src/test/groovy/org/clyze/doop/AndroidTests.groovy
@@ -34,8 +34,8 @@ class AndroidTests extends DoopSpec {
 
 		then:
 		methodIsReachable(analysis, '<jackpal.androidterm.RunScript: void handleIntent()>')
-		varPointsToQ(analysis, '<jackpal.androidterm.RunScript: void handleIntent()>/@this', '<android component object jackpal.androidterm.RunScript>')
-		varValue(analysis, '<jackpal.androidterm.RunScript: void handleIntent()>/@this', '<android component object jackpal.androidterm.RunScript>')
+		varPointsToQ(analysis, '<jackpal.androidterm.RunScript: void handleIntent()>/@this', '<Android component object jackpal.androidterm.RunScript>')
+		varValue(analysis, '<jackpal.androidterm.RunScript: void handleIntent()>/@this', '<Android component object jackpal.androidterm.RunScript>')
 		instanceFieldPointsTo(analysis, '<android.widget.AdapterView$AdapterContextMenuInfo: android.view.View targetView>', '<jackpal.androidterm.Term: jackpal.androidterm.TermView createEmulatorView(jackpal.androidterm.emulatorview.TermSession)>/new jackpal.androidterm.TermView/0')
 		// method reachable due to keep spec
 		methodIsReachable(analysis, '<jackpal.androidterm.TermExec: void <init>(java.util.List)>')
