3611c4db7ad5b0ea14f3ee7ddca1b521d555e771
Anastasios Antoniadis <anantoni@di.uoa.gr>
2026-02-25 21:17:51 +0000

    Remove all artifactory references

--- src/test/groovy/org/clyze/doop/Artifacts.groovy
+++ src/test/groovy/org/clyze/doop/Artifacts.groovy
@@ -3,11 +3,11 @@ package org.clyze.doop
 class Artifacts {
     final static String ANDROIDTERM_APK="https://github.com/plast-lab/doop-benchmarks/raw/refs/heads/main/android-benchmarks/jackpal.androidterm-1.0.70-71-minAPI4.apk"
     final static String ANDROIDTERM_HPROF="https://github.com/plast-lab/doop-benchmarks/raw/refs/heads/main/android-benchmarks/jackpal.androidterm.hprof"
-    final static String PHONOGRAPH_APK = "http://bellatrix.di.uoa.gr:8081/artifactory/Demo-benchmarks/Android/phonograph-gitbuild-commit-dc9189718955cca28c1e7ac06631667d99051538.apk"
-    final static String SIGNAL_APK="http://bellatrix.di.uoa.gr:8081/artifactory/Demo-benchmarks/Android/Signal-play-debug-4.30.4.apk"
+    final static String PHONOGRAPH_APK = "https://github.com/plast-lab/doop-benchmarks/raw/refs/heads/main/android-benchmarks/phonograph-gitbuild-commit-dc9189718955cca28c1e7ac06631667d99051538.apk"
+    final static String SIGNAL_APK="https://github.com/plast-lab/doop-benchmarks/raw/refs/heads/main/android-benchmarks/Signal-play-debug-4.30.4.apk"
     final static String ANTLR_JAR="https://github.com/plast-lab/doop-benchmarks/raw/refs/heads/main/dacapo-2006/antlr.jar"
     final static String HELLO_JAR="https://github.com/plast-lab/doop-benchmarks/raw/refs/heads/main/doop-tests/006-hello-world/build/libs/006-hello-world.jar"
-    final static String GLOVAR_PY="http://bellatrix.di.uoa.gr:8081/artifactory/Demo-benchmarks/python/glovar.py"
+    final static String GLOVAR_PY="https://github.com/plast-lab/doop-benchmarks/raw/refs/heads/main/doop-tests/python-test/glovar.py"
     final static String DROIDBENCH_CLONE1="https://github.com/secure-software-engineering/DroidBench/raw/master/apk/GeneralJava/Clone1.apk"
     final static String DAYTRADER_WAR ="https://github.com/plast-lab/clue-common/raw/64fdc6885ac68a77984d89b9b1a5cb9b24efb9fc/src/test/resources/daytrader-ee7-web.war"
 }
